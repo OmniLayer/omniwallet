ae6e954b3812270cbacdbd519f94a4a26d3d2837
Curtis Lacy <curts@engine.co>
2014-01-15 17:51:59 +0000

    added package.json and gruntfile so that "npm install" checks out the mastercoin-tools.

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+	var initConfig = {
+gitclone: {
+    mastercointools: {
+      options: {
+        // Target-specific options go here.
+repository: 'https://github.com/grazcoin/mastercoin-tools.git',
+                branch: 'master',
+                directory: 'node_modules/mastercoin-tools'
+      }
+    }
+  },	
+};
+
+	grunt.initConfig( initConfig );
+grunt.loadNpmTasks('grunt-git');
+
+	grunt.registerTask( 'default', 'gitclone' );
+};
