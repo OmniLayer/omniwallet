9e1baf091b21a73afa4c1fa448a827387f4a5699
Curtis Lacy <curts@engine.co>
2014-01-16 16:46:50 +0000

    Added validation and file movement steps to the startup.

--- app.sh
+++ app.sh
@@ -6,4 +6,10 @@ mkdir -p $DATADIR
 cd $DATADIR
 mkdir -p tx addr general
 python $APPDIR/node_modules/mastercoin-tools/msc_parse.py
+python $APPDIR/node_modules/mastercoin-tools/msc_validate.py
+
+mkdir -p $DATADIR/www/tx $DATADIR/www/addr $DATADIR/www/general
+cp --no-clobber tx/* $DATADIR/www/tx
+cp --no-clobber addr/* $DATADIR/www/addr
+cp --no-clobber general/* $DATADIR/www/general
 
