0be4fc6e0b8b6c32e275fa723ed0252b38f82e53
Curtis Lacy <curtis@engine.co>
2014-02-18 11:18:22 -0500

    Added asynchronous address requests.

--- www/js/WalletAddressesController.js
+++ www/js/WalletAddressesController.js
@@ -1,5 +1,39 @@
-function WalletAddressesController($scope, $http) {
-	$scope.getCurrencies = function() {
-		console.log( '*** getCurrencies!' );
+function WalletAddressesController($scope, $http , $q) {
+	$scope.getWallet = function() {
+		return {
+			"uuid":"39cd5e05-aa4a-400c-c4c4-9fe70332bd01",
+			"addresses":[ 
+				"13pm7cmA5vVpKkDLJCvqh26kcp6V6PJ1Aq",
+				"1KRZKBqzcqa4agQbYwN5AuHsjvG9fSo2gW"
+			],
+			"keys":[
+				{
+					"address":"13pm7cmA5vVpKkDLJCvqh26kcp6V6PJ1Aq",
+					"encrypted":"NOPE!"
+				},
+				{
+					"address":"1KRZKBqzcqa4agQbYwN5AuHsjvG9fSo2gW",
+					"encrypted":"Still no!"
+				}
+			]
+		};
+	}
+	$scope.getData = function() {
+		var requests = [];
+		var wallet = $scope.getWallet();
+
+		if( wallet )
+			wallet.addresses.forEach( function( addr ) {
+				requests.push( $http.get( '/v1/address/addr/' + addr + '.json' ).then( function( result ) {
+					return result;
+				},
+				function( error ) {
+					return error;
+				} ));
+			});
+		$q.all( requests ).then( function( responses ) {
+			console.log( 'Got responses!' );
+			console.log( responses );
+		} );
 	}
 }
