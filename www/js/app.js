67eb45439db07c33915bf81bf1d4ecdf4938a1a5
Faiz K <faizkhan00@gmail.com>
2014-02-17 20:23:55 -0600

    added history tx

--- www/js/app.js
+++ www/js/app.js
@@ -113,6 +113,43 @@ function BTCController($scope, $http) {
 
 function WalletHistoryController($scope, $http) {
   console.log('initialized wallet history')
+  $scope.getData = function getData(address) {
+    var file = '/v1/address/addr/' + address + '.json'; 
+    $http.get( file, {} ).success(
+      function(data, status, headers, config) {
+
+        $scope.address = data.address;
+
+        delete data.address;
+        delete data.balance;
+        
+        var transaction_data = []
+        angular.forEach(data[0], function(msc_tx, tx_type ) {
+          if( msc_tx instanceof Array && msc_tx.length != 0 ) {
+            console.log(tx_type, msc_tx);
+            transaction_data = transaction_data.concat(msc_tx);
+          }
+        });
+        
+        angular.forEach(data[1], function(tmsc_tx, tx_type) {
+          if( tmsc_tx instanceof Array && tmsc_tx.length != 0 ) {
+            console.log(tx_type, tmsc_tx);
+            transaction_data = transaction_data.concat(tmsc_tx);
+          }
+        });
+
+        //sort by date, ascending
+        transaction_data = transaction_data.sort(function(a,b) {
+            return b.tx_time - a.tx_time;
+        });
+        
+        //DEBUG angular.forEach(transaction_data, function(transaction, index) {
+        //DEBUG console.log(new Date(Number(transaction.tx_time)))
+        //DEBUG });
+
+        $scope.history = transaction_data;
+      });
+  }
 }
 
 function SidecarController($scope, $http) {
