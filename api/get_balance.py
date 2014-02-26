abbc06b841e96bbd8d3f6423448e118e2dd9f5dc
Curtis Lacy <curtis@engine.co>
2014-02-26 20:15:51 +0000

    Moved the mastercoin balance checking into its own function.

--- api/get_balance.py
+++ api/get_balance.py
@@ -8,20 +8,11 @@ from msc_apps import *
 
 data_dir_root = os.environ.get('DATADIR')
 
-def get_balance_response(request_dict):
-  try:
-      addrs_list=request_dict['addr']
-  except KeyError:
-      return (None, 'no address in dictionary')
-      
-  if len(addrs_list)!=1:
-      return response(none, 'no single address')
-  addr=addrs_list[0]
-
+def get_msc_balances( addr ):
   filename = data_dir_root + '/www/addr/' + addr + '.json'
 
   if not os.path.exists(filename):
-    return ( '{ "status": "NOT FOUND: ' + filename + '" }', None );
+    return ( None, '{ "status": "NOT FOUND: ' + filename + '" }' );
 
   with open(filename, 'r') as f:
     address_data = json.load(f)
@@ -32,6 +23,25 @@ def get_balance_response(request_dict):
       if balance_data[ i ][ 'symbol' ] == 'BTC':
         balance_data.pop( i )
         break
+
+  return ( address_data, None )
+  
+
+def get_balance_response(request_dict):
+  try:
+      addrs_list=request_dict['addr']
+  except KeyError:
+      return (None, 'no address in dictionary')
+      
+  if len(addrs_list)!=1:
+      return response(none, 'no single address')
+  addr=addrs_list[0]
+
+  address_data, err = get_msc_balances( addr )
+  if err != None:
+    address_data = {}
+    address_data[ 'address' ] = addr
+    address_data[ 'balance' ] = []
 
   return (json.dumps( address_data ), None)
 
