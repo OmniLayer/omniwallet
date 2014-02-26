1464071b0a6a07a9d90dbc5115dd19fef991fa72
Curtis Lacy <curtis@engine.co>
2014-02-26 19:25:28 +0000

    Added a POST endpoint which gets the balance for an address (currently including only the MSCT and TMSC balances).

---
+++ api/get_balance.py
@@ -0,0 +1,40 @@
+import urlparse
+import os, sys
+import json
+tools_dir = os.environ.get('TOOLSDIR')
+lib_path = os.path.abspath(tools_dir)
+sys.path.append(lib_path)
+from msc_apps import *
+
+data_dir_root = os.environ.get('DATADIR')
+
+def get_balance_response(request_dict):
+  try:
+      addrs_list=request_dict['addr']
+  except KeyError:
+      return (None, 'no address in dictionary')
+      
+  if len(addrs_list)!=1:
+      return response(none, 'no single address')
+  addr=addrs_list[0]
+
+  filename = data_dir_root + '/www/addr/' + addr + '.json'
+
+  if not os.path.exists(filename):
+    return ( '{ "status": "NOT FOUND: ' + filename + '" }', None );
+
+  with open(filename, 'r') as f:
+    address_data = json.load(f)
+    # Once the data's been loaded, remove the BTC entry since we're going to 
+    #    use sx's BTC balances directly.
+    balance_data = address_data[ 'balance' ]
+    for i in (0,len( balance_data )-1):
+      if balance_data[ i ][ 'symbol' ] == 'BTC':
+        balance_data.pop( i )
+        break
+
+  return (json.dumps( address_data ), None)
+
+def get_balance_handler(environ, start_response):
+  return general_handler(environ, start_response, get_balance_response)
+
