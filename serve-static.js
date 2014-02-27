302dbab35bc9996559e0af0d95d22cab189849a3
Curtis Lacy <curtis@engine.co>
2014-02-27 11:17:09 -0500

    Added some handling for a couple of POST endpoints so that we can provide static results for testing.

--- serve-static.js
+++ serve-static.js
@@ -2,15 +2,55 @@ var connect = require('connect'),
 	connectRoute = require( 'connect-route' ),
     http = require('http'),
     url = require( 'url' ),
-    qs = require( 'qs' )
+    qs = require( 'qs' ),
+    fs = require( 'fs' )
+
+var postPaths = [
+	'/v1/user/wallet/sync',
+	'/v1/user/wallet/restore/',
+	'/v1/address/addr/'
+];
 
 connect(
-		connect.bodyParser(),
 		connectRoute( function( app ) {
-			app.post( '/v1', function( req, res ) {
-				console.log( 'POST!' );
-			});
+			postPaths.forEach( function( path ){
+				app.post( path, processPost );
+			} );
 		})
 	)
     .use(connect.static('www'))
     .listen(3000);
+
+function processPost( req, res ) {
+	var body = '';
+        req.on('data', function (data) {
+            body += data;
+        });
+        req.on('end', function () {
+
+            var POST = qs.parse(body);
+            // use POST
+            console.log( 'POST Data: ' );
+
+            if( req.route.indexOf( 'address/addr' ) >= 0 )
+            {
+            	console.log( 'Addr request, address: ' + POST.addr );
+            }
+            else if ( req.route.indexOf( 'wallet/restore' ) >= 0 )
+            {
+            	var file = "www" + req.route + POST.email;
+            	console.log( 'Restore wallet from ' + file );
+            	fs.readFile( file, function (err, data) {
+				  if (err) throw err;
+				  res.write( data );
+				  res.end();
+				});
+            }
+            else
+            {
+            	console.log( 'Unknown submission, data: ' );
+            	console.log( POST );
+            }
+        });
+	res.end();
+}
