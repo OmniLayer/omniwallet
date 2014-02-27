016d534662ade8fa50980646921b054ec94cc4c6
Curtis Lacy <curtis@engine.co>
2014-02-27 10:14:56 -0500

    Started trying to add POST support to the static test harness.

--- serve-static.js
+++ serve-static.js
@@ -1,6 +1,16 @@
 var connect = require('connect'),
-    http = require('http');
+	connectRoute = require( 'connect-route' ),
+    http = require('http'),
+    url = require( 'url' ),
+    qs = require( 'qs' )
 
-connect()
+connect(
+		connect.bodyParser(),
+		connectRoute( function( app ) {
+			app.post( '/v1', function( req, res ) {
+				console.log( 'POST!' );
+			});
+		})
+	)
     .use(connect.static('www'))
     .listen(3000);
