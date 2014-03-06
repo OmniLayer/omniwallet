57560d03009bb8e0c487d687636264a29192decd
Patrick Madden <maddenpj@gmail.com>
2014-03-06 16:37:32 -0600

    Mock out salt endpoint

--- api/user_service.py
+++ api/user_service.py
@@ -1,8 +1,18 @@
-from flask import Flask
+import bcrypt
+from flask import Flask, request, jsonify
 app = Flask(__name__)
+app.debug = True
 
-@app.route('/flask/')
-def hello_world():
-  return 'Hello warld'
+@app.route('/salt')
+def challenge():
+  uuid = request.args.get('uuid', '')
+  salt = bcrypt.hashpw(uuid.encode('UTF-8'), bcrypt.gensalt())
+  random_str = bcrypt.hashpw(bcrypt.gensalt(), bcrypt.gensalt())[7:]
+
+  response = {
+      'salt': salt,
+      'random_str': random_str
+  }
+  return jsonify(response)
 
 
