2e8a705964b8d1f3d74dbcd8df3367611d9be77c
Patrick Madden <maddenpj@gmail.com>
2014-03-12 12:03:25 -0500

    Add update wallet to user service

--- api/user_service.py
+++ api/user_service.py
@@ -3,29 +3,37 @@ import werkzeug.security as ws
 from flask import Flask, request, jsonify, abort, json
 from simplekv.fs import FilesystemStore
 
-LOGIN_DIFFICULTY = '0400'
+ACCOUNT_CREATION_DIFFICULTY = '0400'
+ACCOUNT_CREATION_DIFFICULTY = '0400'
+
 SERVER_SECRET = 'SoSecret!'
+SESSION_SECRET = 'SuperSecretSessionStuff'
 data_dir_root = os.environ.get('DATADIR')
 
 store_dir = data_dir_root + '/sessions/'
 session_store = FilesystemStore(store_dir) # TODO: Need to roll this into a SessionInterface so multiple services can hit it easily
-SESSION_SECRET = 'SuperSecretSessionStuff'
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/salt')
+@app.route('/challenge')
 def challenge():
   uuid = request.args.get('uuid')
   session_id = ws.hashlib.sha256(SESSION_SECRET + uuid).hexdigest()
-  if session_id in session_store:
+  session_id_challenge = session_id + "_challenge"
+  session_id_pow_challenge = session_id + "_pow_challenge"
+
+  if session_id_pow_challenge in session_store:
+    abort(403)
+  if session_id_challenge in session_store:
     abort(403)
 
   salt = ws.hashlib.sha256(SERVER_SECRET + uuid).hexdigest()
   pow_challenge = ws.gen_salt(32)
   challenge = ws.gen_salt(32)
 
-  session_store.put(session_id, pow_challenge)
+  session_store.put(session_id_pow_challenge, pow_challenge)
+  session_store.put(session_id_challenge, challenge)
   response = {
       'salt': salt,
       'pow_challenge': pow_challenge,
@@ -38,18 +46,20 @@ def challenge():
 def create():
   uuid = request.form['uuid']
   session_id = ws.hashlib.sha256(SESSION_SECRET + uuid).hexdigest()
-  if session_id not in session_store:
+  session_id_pow_challenge = session_id + "_pow_challenge"
+
+  if session_id_pow_challenge not in session_store:
     print 'UUID not in session'
     abort(403)
 
   nonce = request.form['nonce']
   public_key = request.form['public_key']
-  pow_challenge = session_store.get(session_id)
+  pow_challenge = session_store.get(session_id_pow_challenge)
   wallet = request.form['wallet']
 
   challenge_response = ws.hashlib.sha256(pow_challenge + nonce).hexdigest()
 
-  if challenge_response[-len(LOGIN_DIFFICULTY):] != LOGIN_DIFFICULTY:
+  if challenge_response[-len(ACCOUNT_CREATION_DIFFICULTY):] != ACCOUNT_CREATION_DIFFICULTY:
     print 'Aborting: Challenge was not met'
     abort(403)
 
@@ -58,7 +68,28 @@ def create():
     abort(403)
 
   write_wallet(uuid, wallet)
-  session_store.delete(session_id)
+  session_store.delete(session_id_pow_challenge)
+
+  return ""
+
+@app.route('/update', methods=['POST'])
+def update():
+  uuid = request.form['uuid']
+  session_id = ws.hashlib.sha256(SESSION_SECRET + uuid).hexdigest()
+  session_id_challenge = session_id + "_challenge"
+
+  if session_id_challenge in session_store:
+    abort(403)
+
+  challenge = request.form['challenge']
+  signature = request.form['signature']
+  wallet = request.form['wallet']
+
+  if challenge != session_store.get(session_id_challenge):
+    abort(403)
+
+  write_wallet(uuid, wallet)
+  session_store.delete(session_id_challenge)
 
   return ""
 
@@ -67,6 +98,12 @@ def login():
   uuid = request.args.get('uuid')
   return ""
 
+def session_get(session_id):
+  raw = session_store.get(session_id)
+  return json.loads(raw)
+
+def session_put(session_id, data_dict):
+  session_store.put(json.dumps(data_dict))
 
 def write_wallet(uuid, wallet):
   filename = data_dir_root + '/wallets/' + uuid + '.json'
