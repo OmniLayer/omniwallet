2537a4df31415337cf856f1b28992a3cca9edb78
valorien <valorien@gmail.com>
2014-03-21 13:55:13 +0200

    Python Idioms fixes - Changed class to "new style" - Added context managers to file I/O operations - Prints now use the format() method - Removed redundant try/except in get() - PEP8 fixes for whitespace and naming

--- lib/stats_file_backend.py
+++ lib/stats_file_backend.py
@@ -1,36 +1,36 @@
 import json
 import threading
-class StatsFileBackend:
-  """
-  This is a implementation of the Stats backend used to store the stats
-  in a simple file. You can change this to a sql backend at a later date
-  if needed.
-  """
-  def __init__(self, options={}):
-    self.lock = threading.Lock()
-    self.db_path = options["db_path"]
-    try:
-      f = open(self.db_path, "r")
-      self.stats = json.loads(f.read())
-      f.close()
-    except (ValueError, IOError) as e:
-      self.stats = {}
-      print "Could not read config, creating new one: " , e
 
-    print "Saving stats in", self.db_path
 
-  def persist(self):
-    f = open(self.db_path, "w")
-    f.write( json.dumps(self.stats) )
-    f.close()
+class StatsFileBackend(object):
 
-  def get(self, key):
-    try:
-      return self.stats[key] 
-    except KeyError:
-      return None
+    """
+    This is an implementation of the Stats backend used to store the stats
+    in a simple file. You can change this to an sql backend at a later date
+    if needed.
+    """
 
-  def put(self, key, val):
-    with self.lock:
-      self.stats[key] = val
-      self.persist()
+    def __init__(self, options={}):
+        self.lock = threading.Lock()
+        self.db_path = options["db_path"]
+
+        try:
+            with open(self.db_path) as db_file:
+                self.stats = json.loads(db_file.read())
+        except (ValueError, IOError) as e:
+            self.stats = {}
+            print("Could not read config, creating new one: {}".format(e))
+
+        print("Saving stats in {}".format(self.db_path))
+
+    def persist(self):
+        with open(self.db_path, "w") as db_file:
+            db_file.write(json.dumps(self.stats))
+
+    def get(self, key):
+        return self.stats.get(key, None)
+
+    def put(self, key, val):
+        with self.lock:
+            self.stats[key] = val
+            self.persist()
