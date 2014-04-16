13b65b7ddcb84d6a214cb3858070c6706b034e5b
Patrick Madden <maddenpj@gmail.com>
2014-04-16 14:54:09 -0500

    Add transaction verification API

--- api/mastercoin_verify.py
+++ api/mastercoin_verify.py
@@ -39,5 +39,52 @@ def addresses():
 
 @app.route('/transactions/<address>')
 def transactions(address=None):
+  currency_id = request.args.get('currency_id')
 
-  return ""
+  print address, currency_id
+
+  if address == None:
+    abort(400)
+
+  if not exists(address):
+    abort(404)
+
+  addr = read(address)
+  transactions = []
+  tx_lists = ['accept_transactions', 'bought_transactions', 'exodus_transactions', 'offer_transactions', 'received_transactions', 'sent_transactions', 'sold_transactions']
+
+  if currency_id == '0':
+    return jsonify({ 'address': address, 'transactions': transactions }) # Punt on bitcoin transactions since we don't store them
+
+  if currency_id == '1' or currency_id == '2':
+    currency_id = str(int(currency_id) - 1) # Mastercoin-tools is off by one on currency id from the spec
+
+  if currency_id in addr:
+    for tx_i in tx_lists:
+      for tx in addr[currency_id][tx_i]:
+        transactions.append(tx_clean(tx))
+
+  return jsonify({ 'address': address, 'transactions': transactions })
+
+
+# Utilities
+def tx_clean(tx):
+  clean = {
+      'tx_hash': tx['tx_hash'],
+      'valid': True,
+      'accepted_amount': tx['formatted_amount']
+  }
+
+  if 'bitcoin_required' in tx:
+    clean['bought_amount'] = tx['bitcoin_required']
+
+  return clean
+
+def read(address):
+  filename = data_dir_root + '/addr/' + address + '.json'
+  with open(filename, 'r') as f:
+    return json.load(f)
+
+def exists(address):
+  filename = data_dir_root + '/addr/' + address + '.json'
+  return os.path.exists(filename)
