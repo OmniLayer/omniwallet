53725db4b5f3bedf011fed489d5eae2ffa9a43f5
Faiz K <faizkhan00@gmail.com>
2014-05-05 13:30:29 -0500

    retab file

--- app.sh
+++ app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 kill_child_processes() {
-	kill $SERVER_PID
-	rm -f $LOCK_FILE
+  kill $SERVER_PID
+  rm -f $LOCK_FILE
 }
 
 # Ctrl-C trap. Catches INT signal
@@ -18,7 +18,7 @@ VALIDATE_LOG=$DATADIR/validated.log
 ARCHIVE_LOG=$DATADIR/archived.log
 
 if [ ! -d $DATADIR/tx ]; then
-	cp -r $TOOLSDIR/www/tx $DATADIR/tx
+  cp -r $TOOLSDIR/www/tx $DATADIR/tx
 fi
 
 # Export directories for API scripts to use
@@ -38,37 +38,37 @@ echo "Beginning main run loop..."
 while true
 do
 
-	# check lock (not to run multiple times)
-	if [ ! -f $LOCK_FILE ]; then
+  # check lock (not to run multiple times)
+  if [ ! -f $LOCK_FILE ]; then
 
-		# lock
-		touch $LOCK_FILE
+    # lock
+    touch $LOCK_FILE
 
-		mkdir -p $DATADIR
-		cd $DATADIR
-		mkdir -p properties tmptx tx addr general offers wallets sessions mastercoin_verify/addresses mastercoin_verify/transactions www
+    mkdir -p $DATADIR
+    cd $DATADIR
+    mkdir -p properties tmptx tx addr general offers wallets sessions mastercoin_verify/addresses mastercoin_verify/transactions www
 
-		# parse until full success
-		x=1 # assume failure
-		echo -n > $PARSE_LOG
+    # parse until full success
+    x=1 # assume failure
+    echo -n > $PARSE_LOG
     echo "Parsing last block $(cat www/revision.json | cut -b 102-109)"
-		while [ "$x" != "0" ];
-		do
-			python $TOOLSDIR/msc_parse.py -r $TOOLSDIR 2>&1 >> $PARSE_LOG
-  			x=$?
-		done
+    while [ "$x" != "0" ];
+    do
+      python $TOOLSDIR/msc_parse.py -r $TOOLSDIR 2>&1 >> $PARSE_LOG
+        x=$?
+    done
     echo "Running validation step..."
-		python $TOOLSDIR/msc_validate.py 2>&1 > $VALIDATE_LOG
-	  
+    python $TOOLSDIR/msc_validate.py 2>&1 > $VALIDATE_LOG
+    
     echo "Getting price calculation..."
     mkdir -p $DATADIR/www/values $DATADIR/www/values/history
     python $APPDIR/api/coin_values.py
-	  
-		# update archive
+    
+    # update archive
     echo "Running archive tool..."
-		python $TOOLSDIR/msc_archive.py -r $TOOLSDIR 2>&1 > $ARCHIVE_LOG
-	
-		mkdir -p $DATADIR/www/tx $DATADIR/www/addr $DATADIR/www/general $DATADIR/www/offers $DATADIR/www/properties $DATADIR/www/mastercoin_verify/addresses $DATADIR/www/mastercoin_verify/transactions
+    python $TOOLSDIR/msc_archive.py -r $TOOLSDIR 2>&1 > $ARCHIVE_LOG
+  
+    mkdir -p $DATADIR/www/tx $DATADIR/www/addr $DATADIR/www/general $DATADIR/www/offers $DATADIR/www/properties $DATADIR/www/mastercoin_verify/addresses $DATADIR/www/mastercoin_verify/transactions
 
     echo "Copying data back to /www/ folder..."
     find $DATADIR/tx/. -name "*.json" | xargs -I % cp -rp % $DATADIR/www/tx
@@ -78,12 +78,12 @@ do
     find $DATADIR/properties/. -name "*.json" | xargs -I % cp -rp % $DATADIR/www/properties
     find $DATADIR/mastercoin_verify/addresses/. | xargs -I % cp -rp % $DATADIR/www/mastercoin_verify/addresses
     find $DATADIR/mastercoin_verify/transactions/. | xargs -I % cp -rp % $DATADIR/www/mastercoin_verify/transactions
-	
-		# unlock
-		rm -f $LOCK_FILE
-	fi
+  
+    # unlock
+    rm -f $LOCK_FILE
+  fi
 
   echo "Done, sleeping..."
-	# Wait a minute, and do it all again.
-	sleep 60
+  # Wait a minute, and do it all again.
+  sleep 60
 done
