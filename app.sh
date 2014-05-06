161f06b0460f321468abceccb545b5de80de749a
Faiz K <faizkhan00@gmail.com>
2014-05-06 18:34:43 -0500

    Add timestamp

--- app.sh
+++ app.sh
@@ -7,6 +7,7 @@ kill_child_processes() {
 
 # Ctrl-C trap. Catches INT signal
 trap "kill_child_processes 1 $$; exit 0" INT
+echo "Starting app.sh: $(TZ='America/Chicago' date)"
 
 echo "Establishing environment variables..."
 APPDIR=`pwd`
