dde28657ef3d359a0dfedff9ac83c700379f96bb
Faiz K <faizkhan00@gmail.com>
2014-05-11 00:14:08 -0500

    Format commit for cryptUtilWorker.js , no errors found.

--- www/js/cryptUtilWorker.js
+++ www/js/cryptUtilWorker.js
@@ -1,5 +1,5 @@
 // sjcl uses the console which isnt available in a web worker
-console = { 
+console = {
   log: function() {}
 }
 
@@ -7,10 +7,10 @@ console = {
 window = this;
 
 importScripts(
-    "/bower_components/jsrsasign/jsrsasign-latest-all-min.js",
-    "/bower_components/forge/js/forge.min.js",
-    "/bower_components/sjcl/sjcl.js",
-    "/js/cryptUtil.js"
+"/bower_components/jsrsasign/jsrsasign-latest-all-min.js",
+"/bower_components/forge/js/forge.min.js",
+"/bower_components/sjcl/sjcl.js",
+"/js/cryptUtil.js"
 );
 
 onmessage = function(oEvent) {
@@ -18,33 +18,34 @@ onmessage = function(oEvent) {
   var result = null;
   var key = null;
   switch (jobDesc.name) {
-   	case "generateNonceForDifficulty":
-   	  result = CryptUtil.generateNonceForDifficulty(jobDesc.challengeString);
-   	  break;
-   	case "generateAsymmetricPair":
-   	  key = CryptUtil.generateAsymmetricPair();
-   	  // workaround for jsrsasign bug#37 (https://github.com/kjur/jsrsasign/issues/37)
-   	  key.privKey.isPrivate = true;
-   	  result = {
-   	      pubPem: key.pubPem,
-   	      privPem: KEYUTIL.getPEM( key.privKey, "PKCS8PRV" )
-   	    };
-   	  break;
-   	case "generateSymmetricKey":
-   	  result = CryptUtil.generateSymmetricKey(jobDesc.password, jobDesc.hexSalt);
-   	  break;
-   	case "encryptObject":
-   	  result = CryptUtil.encryptObject(jobDesc.o, jobDesc.key);
-   	  break;
-   	case "decryptObject":
-   	  result = CryptUtil.decryptObject(jobDesc.string, jobDesc.key);
-   	  break;
-   	case "createSignedObject":
-   	  result = CryptUtil.createSignedObject(jobDesc.data, jobDesc.privKey);
-   	  break;
-   	default:
-   	  break;
+    case "generateNonceForDifficulty":
+      result = CryptUtil.generateNonceForDifficulty(jobDesc.challengeString);
+      break;
+    case "generateAsymmetricPair":
+      key = CryptUtil.generateAsymmetricPair();
+      // workaround for jsrsasign bug#37 (https://github.com/kjur/jsrsasign/issues/37)
+      key.privKey.isPrivate = true;
+      result = {
+        pubPem: key.pubPem,
+        privPem: KEYUTIL.getPEM(key.privKey, "PKCS8PRV")
+      };
+      break;
+    case "generateSymmetricKey":
+      result = CryptUtil.generateSymmetricKey(jobDesc.password, jobDesc.hexSalt);
+      break;
+    case "encryptObject":
+      result = CryptUtil.encryptObject(jobDesc.o, jobDesc.key);
+      break;
+    case "decryptObject":
+      result = CryptUtil.decryptObject(jobDesc.string, jobDesc.key);
+      break;
+    case "createSignedObject":
+      result = CryptUtil.createSignedObject(jobDesc.data, jobDesc.privKey);
+      break;
+    default:
+      break;
   }
-  
+
   postMessage(result);
 }
+
