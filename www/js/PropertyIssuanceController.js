a28249663850889a07ed8e65c945dd5f04771772
Tomás Corellano <hack.rapture@gmail.com>
2014-05-24 16:33:36 -0300

    Initial frontend commit

---
+++ www/js/PropertyIssuanceController.js
@@ -0,0 +1,26 @@
+function PropertyIssuanceController($scope, $http){
+  $scope.issueProperty = function(){
+    var request = { 
+      "transaction_type": $scope.transaction_type,
+      "ecosystem": $scope.ecosystem,
+      "property_type": $scope.property_type,
+      "previous_property_id": $scope.previous_property_id,
+      "property_category": $scope.property_category,
+      "property_subcategory": $scope.property_subcategory,
+      "property_name": $scope.property_name,
+      "property_url": $scope.property_url,
+      "property_data": $scope.property_data,
+      "number_properties": $scope.number_properties,
+      "transaction_from": $scope.transaction_from,
+      "from_private_key": $scope.from_private_key
+    };
+    $http({
+          url: "/v1/properties/issue",
+          method: 'POST',
+          data:  request
+          })
+    .then(function(result){
+        
+    });
+  };
+}
