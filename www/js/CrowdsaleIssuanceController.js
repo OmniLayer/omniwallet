d24ae122c8f7e4482f526e23af763084481cbc57
Tomás Corellano <hack.rapture@gmail.com>
2014-06-08 03:52:13 -0300

    Added datepicker control

--- www/js/CrowdsaleIssuanceController.js
+++ www/js/CrowdsaleIssuanceController.js
@@ -127,4 +127,41 @@ function CrowdsaleIssuanceController($scope, $http,$modal, userService){
       $scope.$parent.showErrors = true;
     }
   };
+  
+  
+  // DATEPICKER OPTIONS
+  $scope.today = function() {
+    $scope.deadline= new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.deadline = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.initDate = new Date('2016-15-20');
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
 }
