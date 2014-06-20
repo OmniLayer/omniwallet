1a798657931db3847951f77e895469927c101933
Tomás Corellano <hack.rapture@gmail.com>
2014-06-20 12:29:34 -0300

    Cleanup datepicket options

--- www/js/CrowdsaleIssuanceController.js
+++ www/js/CrowdsaleIssuanceController.js
@@ -184,20 +184,6 @@ function CrowdsaleIssuanceController($scope, propertiesService){
   };
   $scope.today();
 
-  $scope.clear = function () {
-    $scope.deadline = null;
-  };
-
-  // Disable weekend selection
-  $scope.disabled = function(date, mode) {
-    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-  };
-
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
-  $scope.toggleMin();
-
   $scope.open = function($event) {
     $event.preventDefault();
     $event.stopPropagation();
@@ -209,8 +195,6 @@ function CrowdsaleIssuanceController($scope, propertiesService){
     formatYear: 'yy',
     startingDay: 1
   };
-
-  $scope.initDate = new Date('2016-15-20');
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
+  
+  $scope.format = 'dd-MMMM-yyyy';
 }
