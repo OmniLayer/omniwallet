fcafb791f272da7d849bfbaf8dcb53fb027c8a84
Tomás Corellano <hack.rapture@gmail.com>
2014-06-25 20:37:10 -0300

    tx51 navigation routes and button.

--- www/js/app.js
+++ www/js/app.js
@@ -18,7 +18,7 @@ var app = angular.module('omniwallet', [
   $routeProvider.when('/assets/:page?', {
       templateUrl: function(route) {
         //new views added here
-        var availableViews = ['issue'];
+        var availableViews = ['issue','crowdsale'];
 
         var view;
         var viewFound = availableViews.indexOf(route.page);
