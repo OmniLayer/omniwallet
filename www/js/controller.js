de844c3d99d01440e7ccef7065d5d1fc441595e9
Faiz Khan <faizkhan00@gmail.com>
2014-06-25 08:23:59 -0500

    Add results for, sorting, visual fix

--- www/js/controller.js
+++ www/js/controller.js
@@ -198,6 +198,8 @@ function ExplorerController($scope, $http, hashExplorer) {
   $scope.setHashExplorer = hashExplorer.setHash.bind(hashExplorer);
   // Scope members
   $scope.searchQueryText = '';
+  $scope.searchRan = false;
+  $scope.searchQueryReturned=false;
   $scope.transactions = [];
   $scope.currencies = ['MSC', 'TMSC'];
   $scope.currency = 'MSC';
@@ -225,13 +227,22 @@ function ExplorerController($scope, $http, hashExplorer) {
   $scope.doSearch = function() {
     if( $scope.searchQueryText == undefined || $scope.searchQueryText == '' || $scope.searchQueryText.length < 4 )
       return -1;
+    $scope.searchRan=true;
+    $scope.searchQueryReturned=false;
     var file = '/v1/search/';
     $http.get('/v1/search/?query=' + $scope.searchQueryText, {}).success(function(successData, status, headers, config) {
       angular.forEach(successData.data, function(transaction, index) {
         //DEBUG console.log(transaction)
         successData.data[index].tx_hash_concat = transaction.tx_hash.substring(0, 22) + '...'
       });
+
+      //Sort by date
+      successData.data = successData.data.sort(function(a, b) {
+        return b.tx_time - a.tx_time;
+      });
       $scope.transactions = successData.data;
+      $scope.searchQueryReturned=true;
+      $scope.searchQueryReturnedText=$scope.searchQueryText
     });
   };
   $scope.searchQuery = function(trans) {
