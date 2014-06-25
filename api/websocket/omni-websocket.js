4d14ceff6ea906bed601c21caa621bf7d4d6b1bc
Faiz Khan <faizkhan00@gmail.com>
2014-06-25 09:25:56 -0500

    Fix to nginx conf, change port to match, redo index.html test

--- api/websocket/omni-websocket.js
+++ api/websocket/omni-websocket.js
@@ -14,6 +14,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(1089, function(){
+  console.log('listening on *:1089');
 });
