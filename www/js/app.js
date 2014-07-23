8b357065048b4d16900845e8d5f9589a88782ef4
Faiz Khan <faizkhan00@gmail.com>
2014-07-23 15:52:01 -0500

    Add testnet flag

--- www/js/app.js
+++ www/js/app.js
@@ -15,8 +15,10 @@ var app = angular.module('omniwallet', [
   $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
   $httpProvider.defaults.transformRequest = [TransformRequest];
 
-  if (document.location.href.match('testnet') != null)
+  if (document.location.href.match('testnet') != null) {
     Bitcoin.setNetwork('test');
+    TESTNET=true;
+  }
 
   $routeProvider.when('/assets/:page?', {
       templateUrl: function(route) {
