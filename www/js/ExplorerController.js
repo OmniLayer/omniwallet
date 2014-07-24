80d97aa1b71aab865b07adc3ba2ecd965d0b5782
Tomás Corellano <hack.rapture@gmail.com>
2014-07-24 03:36:39 -0300

    Add some place holders to access the details page

--- www/js/ExplorerController.js
+++ www/js/ExplorerController.js
@@ -1,5 +1,13 @@
-function ExplorerAssetsController($scope, userService){
+function ExplorerAssetsController($scope, userService, propertiesService){
   $scope.isLoggedIn = userService.loggedIn();
+  $scope.ecosystem = 2;
+  $scope.crowdsales = [];
+  $scope.setEcosystem = function(){
+    propertiesService.getActiveCrowdsales($scope.ecosystem).then(function(result){
+      $scope.crowdsales=result.data.crowdsales;
+    });
+  };
   
-  //TODO: Get assets data for display
+  $scope.setEcosystem();
+  
 }
