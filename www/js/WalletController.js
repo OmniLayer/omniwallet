6afa9d5952255889e1aa991e0ce4c812abdaf440
Adam <shmali81@gmail.com>
2014-08-21 14:57:43 -0500

    Fix offers timeframe for Pending Page

--- www/js/WalletController.js
+++ www/js/WalletController.js
@@ -434,7 +434,10 @@ function WalletTradePendingController($scope, $http, $q, userService, hashExplor
     }
   });
   
-  $scope.selectedTimeframe = "604800"
+  //Get All data
+  $scope.timeNow = Date.now();
+  $scope.selectedTimeframe = $scope.timeNow - 1394834400000;
+  //$scope.selectedTimeframe = "604800"
   $scope.filterData = function(time) {
     var orderbook = JSON.parse($scope.orderBookStorage);
 
