5696640b29670724472cbe1d139936f7e44cb197
Tomás Corellano <hack.rapture@gmail.com>
2014-09-17 19:44:12 -0300

    SQL optimisations.

--- api/properties_service.py
+++ api/properties_service.py
@@ -78,8 +78,8 @@ def list():
         query += " AND issuer='" + str(issuer) + "'"
     except KeyError:
         issuer = ""
-    
-    sqlconn.execute("select * from smartproperties where PropertyID not in (1,2) AND " + str(query))
+        
+    sqlconn.execute("select * from smartproperties where PropertyID > 2 AND " + str(query))
     ROWS= sqlconn.fetchall()
     data=[]
     for property in ROWS:
