789527c1cbc71bf5450bc7b90dcd62497fbb08d7
NadavSch <nadav658@gmail.com>
2014-09-28 17:36:49 +0000

    issue 772 fixed

--- www/js/PropertyIssuanceController.js
+++ www/js/PropertyIssuanceController.js
@@ -1,4 +1,4 @@
-function PropertyIssuanceController($scope, propertiesService){
+function PropertyIssuanceController($scope, propertiesService, $timeout){
   $scope.walletAssets=$scope.$parent.$parent;
   $scope.walletAssets.currencyList.forEach(function(e, i) {
     if (e.symbol == "BTC")
@@ -42,7 +42,6 @@ function PropertyIssuanceController($scope, propertiesService){
     $scope.tokenStep = $scope.tokenMin = $scope.isDivisible() ? 0.00000001 : 1;
     $scope.tokenMax = $scope.isDivisible() ? "92233720368.54775807" : "9223372036854775807";
   };
-  
   transactionGenerationController.validateTransactionData=function(){
     var dustValue = 5757;
     var minerMinimum = 10000;
@@ -88,7 +87,24 @@ function PropertyIssuanceController($scope, propertiesService){
     $modalScope.selectedAddress= $scope.selectedAddress;
     $modalScope.minerFees= +$scope.convertDisplayedValue($scope.minerFees);
     $modalScope.totalCost= +$scope.convertDisplayedValue($scope.totalCost);
-    $modalScope.expanded = false;
+    $modalScope.expanded = true;
+    $modalScope.rendered = false;
+    $modalScope.setExpandableDiv = function(){
+      $timeout(function(){
+        $scope.$apply(function(){
+          var offsetHeight = document.getElementById('expandable-div').offsetHeight;
+          var lines = offsetHeight/25;
+          if (lines > 2) {
+            $modalScope.longText = true;
+            $modalScope.expanded = false;
+          }
+          else {
+            $modalScope.longText = false;
+            $modalScope.expanded = true;
+          } 
+        });   
+      },0,false);  
+    }
   };
   
   transactionGenerationController.generateData = function(){
