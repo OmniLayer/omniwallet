74dfa56078d842773f175c84426870db70a9c719
Adam <shmali81@gmail.com>
2014-09-29 15:54:49 -0500

    Start to cleanup old tools

--- api/offers.py
+++ api/offers.py
@@ -1,9 +1,9 @@
 import urlparse
 import os, sys, tempfile, json
 import glob,time
-tools_dir = os.environ.get('TOOLSDIR')
-lib_path = os.path.abspath(tools_dir)
-sys.path.append(lib_path)
+#tools_dir = os.environ.get('TOOLSDIR')
+#lib_path = os.path.abspath(tools_dir)
+#sys.path.append(lib_path)
 from msc_apps import *
 from decimal import *
 
