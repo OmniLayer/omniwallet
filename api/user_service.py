b2da53b1806ba27e9e7396b710210f2413bc8185
Adam <shmali81@gmail.com>
2014-10-20 16:29:13 -0500

    Update Warning message

--- api/user_service.py
+++ api/user_service.py
@@ -347,7 +347,9 @@ def email_wallet(user_email, wallet, uuid):
             'With this file you can import your wallet into any server running the Omniwallet software\n'
             'Be sure to keep this safe and stored seperately from your password\n\n'
             'Wallet Id: '+str(uuid)+'\n'
-            'Login Link: https://'+str(email_domain)+'/login/'+str(uuid)  )
+            'Login Link: https://'+str(email_domain)+'/login/'+str(uuid)+'\n\n\n'
+            'Do not Forget Your Password!\n'
+            'WARNING: Forgotten passwords are UNRECOVERABLE and will results in LOSS of ALL of your coins!' )
 
     html = ('<html><head></head>'
             '<img src="https://'+str(email_domain)+'/assets/img/logo.png"><h2>Omniwallet Backup File</h2>'
@@ -356,7 +358,9 @@ def email_wallet(user_email, wallet, uuid):
             'With this file you can import your wallet into any server running the Omniwallet software.<br>'
             'Be sure to keep this safe and stored seperately from your password<br><br>'
             '<b>Wallet Id:</b> '+str(uuid)+'<br>'
-            '<b>Login Link:</b> <a href="https://'+str(email_domain)+'/login/'+str(uuid)+'">https://'+str(email_domain)+'/login/'+str(uuid)+'<br>'
+            '<b>Login Link:</b> <a href="https://'+str(email_domain)+'/login/'+str(uuid)+'">https://'+str(email_domain)+'/login/'+str(uuid)+'<br><br><br>'
+            '<h3>Do not Forget Your Password!</h3><br>'
+            '<b>WARNING:</b> Forgotten passwords are UNRECOVERABLE and will results in LOSS of ALL funds in your wallet not backed up!<br>'
             '</p></body></html>'  )
 
     part1 = MIMEText(text, 'plain')
