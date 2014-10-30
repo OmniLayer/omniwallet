4e43b8dbf5c18f8d0874aec8e681094d98058513
Tomás Corellano <hack.rapture@gmail.com>
2014-10-30 17:41:57 -0300

    Refactor - keep the config module on a separate file

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,3 @@
-//global config goes here
-angular.module("omniConfig", [])
-  .constant("idleDuration", 10 * 60) // 10 minutes
-  .constant("idleWarningDuration", 2 * 60) // 2 minutes
-  .constant("reCaptchaKey", "----KEY----")
-  .value("SATOSHI_UNIT", new Big(100000000)) //Backend data needs satoshi, use this conversion ratio
-  .value("MIN_MINER_FEE", new Big(0.00010000))
-  .value("MSC_PROTOCOL_COST",0.00025)
-  .value("WHOLE_UNIT",new Big(0.00000001));
 //Define Modules here first
 angular.module("omniServices", ["omniConfig"]);
 
