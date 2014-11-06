919f5d201c50ed48e890be2a825fbbe0dd999a1f
Tomás Corellano <hack.rapture@gmail.com>
2014-11-05 23:21:38 -0300

    Add DEx phase 2 view and controller

---
+++ www/js/controllers/DEx/SaleController.js
@@ -0,0 +1,47 @@
+angular.module("omniControllers")
+	.controller("DExSaleController", ["$scope","TransactionManager","WalletAssets","ModalManager", "AccountManager",
+		function DExSaleController($scope, TransactionManager, WalletAssets, ModalManager, AccountManager ){
+			$scope.walletAssets = WalletAssets;
+
+			$scope.DExSaleTransaction = new TransactionManager(21);
+			$scope.setEcosystem = AccountManager.setEcosystem;
+
+			$scope.setSelectedCoin = function(currencyId){
+				WalletAssets.currencyList.forEach(function(currency){
+					if(currency.id == currencyId)
+						WalletAssets.selectedCoin = currency;
+				});
+			}
+
+			$scope.validateDexSaleForm = function(){
+				// TODO: Validations
+				ModalManager.openConfirmationModal({
+					title:"Confirm DEx Sale",
+					dataTemplate: '/views/modals/partials/dex_sale.html',
+					scope: {
+						address:WalletAssets.selectedAddress,
+						saleCurrency:$scope.saleCurrency,
+						saleAmount:$scope.saleAmount,
+						desiredCurrency:$scope.desiredCurrency,
+						desiredAmount:$scope.desiredAmount,
+						action:$scope.action,
+						totalCost:WalletAssets.totalCost,
+						convertSatoshiToDisplayedValue:WalletAssets.convertSatoshiToDisplayedValue
+					},
+					fromAddress: WalletAssets.selectedAddress,
+					confirmText: "Create Sale",
+					confirm: function(){
+						var data = TransactionManager.prepareData({
+							transaction_version:0,
+							sale_currency_id:$scope.saleCurrency,
+							sale_amount:$scope.saleAmount,
+							desired_currency:$scope.desiredCurrency,
+							desired_amount:$scope.desiredAmount,
+							action:$scope.action,
+						},WalletAssets.selectedAddress)
+
+						return TransactionManager.processTransaction(data,WalletAssets.offline)
+					}
+				})
+			};
+		}])
