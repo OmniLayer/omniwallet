438d33641d64f6ff76ee8ed447de47c727df5f62
Adam <shmali81@gmail.com>
2014-11-07 17:14:00 -0600

    add order data to overview

--- www/js/controllers/DEx/OverviewController.js
+++ www/js/controllers/DEx/OverviewController.js
@@ -1,4 +1,7 @@
 angular.module("omniControllers")
 	.controller("DExOverviewController", ["$scope","userService", function DExOverviewController($scope,userService){
 		$scope.isLoggedIn = userService.loggedIn();
+                $http.get('/v1/exchange/orders').success(function(data) {
+                  $scope.orders=data;
+                });
 	}]);
