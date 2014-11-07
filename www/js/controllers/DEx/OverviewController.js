b012327211a9257cb73df547b27ef4d88ed42500
Adam <shmali81@gmail.com>
2014-11-07 17:19:15 -0600

    add missing directive

--- www/js/controllers/DEx/OverviewController.js
+++ www/js/controllers/DEx/OverviewController.js
@@ -1,5 +1,5 @@
 angular.module("omniControllers")
-	.controller("DExOverviewController", ["$scope","userService", function DExOverviewController($scope,$http,userService){
+	.controller("DExOverviewController", ["$scope","$http","userService", function DExOverviewController($scope,$http,userService){
 		$scope.isLoggedIn = userService.loggedIn();
                 $http.get('/v1/exchange/orders').success(function(data) {
                   $scope.orders=data;
