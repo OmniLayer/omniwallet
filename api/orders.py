13d0ea0cec0c42c0944778f3674ee056f6f4d265
Adam <shmali81@gmail.com>
2014-11-07 16:55:20 -0600

    change to flask

--- api/orders.py
+++ api/orders.py
@@ -1,14 +1,14 @@
-import urlparse
-import os, sys, tempfile, json
-import glob,time
+from flask import Flask, request, jsonify, abort, json
+#import psycopg2, psycopg2.extras
 from msc_apps import *
-from decimal import *
 
-def orders_response(response_dict):
-    maxblock=dbSelect("select max(blocknumber) from orderblob")[0][0]
-    orders=json.loads(dbSelect("select orders from orderblob where blocknumber=%s",[maxblock])[0][0])
+app = Flask(__name__)
+app.debug = True
 
-    return (orders, None)
+@app.route('/v1/exchange/orders')
+def orders():
 
-def orders_handler(environ, start_response):
-    return general_handler(environ, start_response, orders_response)
+  maxblock=dbSelect("select max(blocknumber) from orderblob")[0][0]
+  orders=json.loads(dbSelect("select orders from orderblob where blocknumber=%s",[maxblock])[0][0])
+
+  return (orders, None)
