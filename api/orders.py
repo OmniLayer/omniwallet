8446882a4ec4210890a3822df8d619163935c484
Adam <shmali81@gmail.com>
2014-11-07 17:04:47 -0600

    Fix json error

--- api/orders.py
+++ api/orders.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify, abort, json
-#import psycopg2, psycopg2.extras
 from msc_apps import *
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/v1/exchange/orders')
+@app.route('/')
 def orders():
 
   maxblock=dbSelect("select max(blocknumber) from orderblob")[0][0]
   orders=json.loads(dbSelect("select orders from orderblob where blocknumber=%s",[maxblock])[0][0])
 
-  return (orders, None)
+  return json.dumps(orders)
+
