3c610ecd3ad242d234e494d0c03737951e1c18c6
Adam <shmali81@gmail.com>
2014-11-10 15:41:24 -0600

    abstract smtp server settings

--- api/config.py
+++ api/config.py
@@ -1,5 +1,12 @@
 RECAPTCHA_PRIVATE = None #replace private key text here
-DOMAIN = None  #Replace with domain to override email domain lookup
+
+#email settings
+DOMAIN = None     #Replace with domain to override email domain lookup, otherwise system hostname is used
+EMAILFROM = None  #Is set to None, use noreply@domain
+SMTPUSER = None   #If your smtp server requires authentication define it here
+SMTPPASS = None   #If your smtp server requires authentication define it here
+SMTPDOMAIN = 'localhost'  #smtp server to use for sending, default    'localhost'
+SMTPPORT = 25     #smtp port,  default 25
 
 #For wallets and session store you can switch between disk and the database
 LOCALDEVBYPASSDB = 0    #Set to 1 to use local storage/file system, Set to 0 to use database
