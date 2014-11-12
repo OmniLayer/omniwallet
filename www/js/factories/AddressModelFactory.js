e88fc30717f3f5bda27833fa79c8de9c2451d6a8
Tomás Corellano <hack.rapture@gmail.com>
2014-11-12 03:32:26 -0300

    Refactor - add the AddressModel and BalanceSocket functionality

--- www/js/factories/AddressModelFactory.js
+++ www/js/factories/AddressModelFactory.js
@@ -1,14 +1,20 @@
 angular.module("omniFactories")
-	.factory("AddressModel",[function AddressModelFactory(){
-		var AddressModel = function(address,privkey='',pubkey=''){
+	.factory("AddressModel", ["BalanceSocket", function AddressModelFactory(BalanceSocket){
+		var AddressModel = function(address,privkey,pubkey){
 			var self = this;
 
 			self.initialize = function(){
-				self.socket = io.connect('http://' + document.domain + ':' + location.port + "/balance");
+				self.address = address;
+				self.privkey = privkey;
+				self.pubkey = pubkey;
 
-				self.socket.on("update:"+address, function(data){
+				self.socket = BalanceSocket;
+
+				self.socket.on("address:"+address, function(data){
 					angular.extend(self,data)
 				});
+
+				self.socket.emit("address:add", {data:address});
 			}
 			self.initialize();
 		}
---
+++ www/js/services/BalanceSocket.js
@@ -0,0 +1,12 @@
+angular.module("omniServices")
+	.service("BalanceSocket", ["$rootScope",function BalanceSocketService($rootScope){
+		var self = this;
+
+		self.socket = io.connect('http://' + document.domain + ':' + location.port + "/balance");
+
+		self.on = function(eventName,callback){
+			self.socket.on(eventName,function(msg){
+				$rootScope.apply(function(){callback(msg)});
+			})
+		}
+	}])
