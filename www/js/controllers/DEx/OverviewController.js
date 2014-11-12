df06aec17668744da5227d24f9f8761957f34557
Tomás Corellano <hack.rapture@gmail.com>
2014-11-12 15:23:58 -0300

    Added orderbook tabs

--- www/js/controllers/DEx/OverviewController.js
+++ www/js/controllers/DEx/OverviewController.js
@@ -1,4 +1,18 @@
 angular.module("omniControllers")
-	.controller("DExOverviewController", ["$scope","userService", function DExOverviewController($scope,userService){
-		$scope.isLoggedIn = userService.loggedIn();
+	.controller("DExOverviewController", ["$scope","userService","Orderbook","TransactionManager",
+		function DExOverviewController($scope,userService,Orderbook,TransactionManager){
+			$scope.isLoggedIn = userService.loggedIn();
+			$scope.orderbooks = [];
+
+			var DExSaleTransaction = new TransactionManager(21);
+
+			$scope.switchOrderbook = function(propertyId){
+				$scope.orderbooks.forEach(function(orderbook){
+					if(orderbook.tradingPair.property == propertyId)
+						return orderbook.active = true;
+				});
+				var tradingPair = {property:propertyId,pair: propertyId < 2147483651 ? 1 : 2};
+				var orderbook = new Orderbook(DExSaleTransaction, tradingPair)
+				$scope.orderbooks.push(orderbook);
+			}
 	}]);
