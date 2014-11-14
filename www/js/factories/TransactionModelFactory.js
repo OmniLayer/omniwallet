118a5e79bdf1147cfe9e99d19c7b45f91745af17
Tomás Corellano <hack.rapture@gmail.com>
2014-11-14 12:49:50 -0300

    Refactor - Define TransactionModel and adjust manager services to work with it

---
+++ www/js/factories/TransactionModelFactory.js
@@ -0,0 +1,34 @@
+angular.module("omniFactories")
+	.factory("Transaction",["TESTNET",function TransactionModelFactory(TESTNET){
+		var TransactionModel = function(txType,fromAddress,minerFee,txData){
+			var self = this;
+
+			self.initialize = function(){
+				self.type = txType;
+
+				if(fromAddress.privkey){
+					self.privKey = new Bitcoin.ECKey.decodeEncryptedFormat(fromAddress.privkey, fromAddress.address); // Using address as temporary password
+                    self.pubKey = privKey.getPubKeyHex();
+
+					self.offline = false;
+				}else if(fromAddress.pubkey){
+					self.pubKey = fromAddress.pubkey.toUpperCase();
+
+					self.offline = true;
+				}else
+					throw "Transaction needs an Address with private or public key"; // Throw an error?
+
+				self.address = fromAddress;
+				self.data = txData;
+
+                self.data['pubkey'] = self.pubKey;
+                self.data['fee']= minerFee;
+                self.data['transaction_from'] = self.address.address;
+                self.data['testnet'] = TESTNET || false;
+			}
+
+			self.initialize()
+		}
+
+		return TransactionModel;
+	}])
