a49b73fe6ffd9c857631f4b359aadb722e04f762
Tomás Corellano <hack.rapture@gmail.com>
2014-11-17 19:06:50 -0300

    Refactor - add create wallet functionality

--- www/js/services/AccountService.js
+++ www/js/services/AccountService.js
@@ -2,6 +2,94 @@ angular.module("omniServices")
     .service("Account", ["$http", "$q", "Address","Wallet", function AccountService($http, $q, Address, Wallet) {
         var self = this;
 
+        function generateUUID() {
+          var d = new Date().getTime();
+          var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+          });
+          return uuid;
+        };
+
+
+        self.verifyUUID = function(uuid) {
+          //Check UUID for proper format
+          verify = uuid.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89a-f][0-9a-f]{3}-[0-9a-f]{12}$/i) || []
+          
+          //Return false if it fails, true if its valid structure
+          if (verify.length == 0) {
+           return false;
+          } else {
+           return true;
+          }
+        };
+
+        self.create = function(create){
+            var create = $q.defer();
+            if(!self.validating){
+                self.uuid = generateUUID();
+                var wallet = {
+                  uuid: self.uuid,
+                  addresses: []
+                };
+                var walletKey = '';
+                var asymKey = {};
+                self.validating=true;
+                $http.get('/v1/user/wallet/challenge?uuid=' + uuid)
+                  .then(function(result) {
+                    var data = result.data;
+                    self.nonce = CryptUtil.generateNonceForDifficulty(data.pow_challenge);
+                    self.walletKey = CryptUtil.generateSymmetricKey(create.password, data.salt);
+                    var encryptedWallet = CryptUtil.encryptObject(wallet, walletKey);
+                    self.asymKey = CryptUtil.generateAsymmetricPair();
+                    var createData = {
+                        email: create.email,
+                        nonce: self.nonce,
+                        public_key: self.asymKey.pubPem,
+                        uuid: self.uuid,
+                        wallet: encryptedWallet
+                      };
+
+                    if(create.captcha){
+                      angular.extend(createData, {
+                        recaptcha_challenge_field:create.captcha.challenge,
+                        recaptcha_response_field:create.captcha.response
+                      })
+                    };
+
+                    return $http({
+                      url: '/v1/user/wallet/create',
+                      method: 'POST',
+                      data: createData
+                    });
+                  })
+                  .then(function(result) {
+                    if(result.data.error =="InvalidCaptcha"){
+                      
+                      self.validating=false;
+                      Recaptcha.reload();
+                      create.reject({
+                        invalidCaptcha : true
+                      })
+                    }else {
+                      self.validating=false;
+
+                      Wallet.initialize(wallet)
+                      ga('send', 'event', 'button', 'click', 'Create Wallet');
+                      create.resolve(self);
+                    }
+                  }, function(result) {
+                    self.validating = false;
+                    create.reject({
+                        serverError : true
+                    });
+                  });
+            }
+            
+            return create.promise;
+        }
+
         self.login = function(uuid, passphrase) {
             var login = $q.defer()
             if (!self.loggedIn && !self.loginInProgress) {
