881479b6ca5e233cd51cadb05e186e2f3c128b5b
Adam <shmali81@gmail.com>
2014-11-17 21:40:16 -0600

    add updated api for new orderbook

--- api/orders.py
+++ api/orders.py
@@ -11,3 +11,65 @@ def orders():
   orders=json.loads(dbSelect("select orders from orderblob where blocknumber=%s",[maxblock])[0][0])
 
   return jsonify({ 'orders': orders })
+
+@app.route('/book')
+def orderbook():
+
+   orders=dbSelect("select seller,propertyforsale,amountforsale,remainingforsale,propertydesired,amountdesired,remainingdesired "
+                   "from orderbook where orderstate='open' or orderstate='open-part-filled'")
+
+   response_status='OK'
+   response='{"status":"'+response_status+'", "data":'+ str(orders) +'}'
+
+   return (response, None)
+
+@app.route('/book/all')
+def orderbookall():
+
+   orders=dbSelect("select seller,propertyforsale,amountforsale,remainingforsale,propertydesired,amountdesired,remainingdesired,orderstate "
+                   "from orderbook")
+
+   response_status='OK'
+   response='{"status":"'+response_status+'", "data":'+ str(orders) +'}'
+
+   return (response, None)
+
+@app.route('/book/address/<address>')
+def orderbookbyaddress(address):
+
+   print "hello"
+   print address
+
+   try:
+     address = re.sub(r'\W+', '', address) #check alphanumeric
+     orders=dbSelect("select seller,propertyforsale,amountforsale,remainingforsale,propertydesired,amountdesired,remainingdesired,orderstate "
+                     "from orderbook where seller=%s",[address])
+
+     response_status='OK'
+     response='{"status":"'+response_status+'", "data":'+ str(orders) +'}'
+   except TypeError:
+     response_status='Not OK'
+     response='{"status":"'+response_status+'", "data": No Addresses provided}'
+
+
+   return (response, None)
+
+@app.route('/book/pair/<int:currency1>/<int:currency2>')
+def orderbookbypair(currency1=None,currency2=None):
+
+   try:
+     currency1 = re.sub(r'\D', '', str(currency1)) #check alphanumeric
+     currency2 = re.sub(r'\D', '', str(currency2)) #check alphanumeric
+
+     orders=dbSelect("select seller,propertyforsale,amountforsale,remainingforsale,propertydesired,amountdesired,remainingdesired,orderstate "
+                     "from orderbook where propertyforsale=%s and propertydesired=%s",(currency1,currency2))
+
+     response_status='OK'
+     response='{"status":"'+response_status+'", "data":'+ str(orders) +'}'
+   except TypeError:
+     response_status='Not OK'
+     response='{"status":"'+response_status+'", "data": No currency provided}'
+
+
+   return (response, None)
+
