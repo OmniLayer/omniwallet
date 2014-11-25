a350bcf082a4077d3df608466736e80624cd2249
Tomás Corellano <hack.rapture@gmail.com>
2014-11-24 23:53:46 -0300

    Get balances from the Address models for My Addresses page

--- www/js/WalletAddressesController.js
+++ www/js/WalletAddressesController.js
@@ -28,93 +28,86 @@ angular.module('omniwallet')
             var appraiser = $injector.get('appraiser');
 
             wallet.addresses.forEach(function(addr) {
-              requests.push($injector.get('balanceService').balance(addr.address).then(function(result) {
-                if (result.data.balance.length == 0) {
-                  console.log('No balances for ' + addr.address + ', invalid address?');
-                  invalidAddresses.push(addr.address);
-                } else {
-                  result.data.balance.forEach(function(currencyItem) {
-                    if(currencyItem.divisible)
-                      var value=new Big(currencyItem.value).times(WHOLE_UNIT).valueOf();
-                    if (!balances.hasOwnProperty(currencyItem.symbol)) {
-                      balances[currencyItem.symbol] = {
-                        "symbol": currencyItem.symbol,
-                        "balance": +value || currencyItem.value,
-                        "value": appraiser.getValue(currencyItem.value, currencyItem.symbol, currencyItem.divisible),
-                        "addresses": {}
-                      };
-                    } else {
-                      balances[currencyItem.symbol].balance += +value || currencyItem.value;
-                      balances[currencyItem.symbol].value += appraiser.getValue(currencyItem.value, currencyItem.symbol, currencyItem.divisible);
-                    }
-
-                    if (currencyItem.symbol == 'BTC') {
-                      balances[currencyItem.symbol].name = "Bitcoin"
-                    }
-            		    if (addr.privkey) {
-            			   hasPrivate=true;
-            		    } else {
-            			   hasPrivate=false;
-            		    }
-            		    if (addr.pubkey) {
-                     isOffline=true;
-                    } else {
-                     isOffline=false;
-                    }
-                    balances[currencyItem.symbol].addresses[result.data.address] = {
-                      "address": result.data.address,
+              if (addr.balance.length == 0) {
+                console.log('No balances for ' + addr.address + ', invalid address?');
+                invalidAddresses.push(addr.address);
+              } else {
+                addr.balance.forEach(function(currencyItem) {
+                  if(currencyItem.divisible)
+                    var value=new Big(currencyItem.value).times(WHOLE_UNIT).valueOf();
+                  if (!balances.hasOwnProperty(currencyItem.symbol)) {
+                    balances[currencyItem.symbol] = {
+                      "symbol": currencyItem.symbol,
                       "balance": +value || currencyItem.value,
                       "value": appraiser.getValue(currencyItem.value, currencyItem.symbol, currencyItem.divisible),
-		                  "private": hasPrivate,
-		                  "offline": isOffline
+                      "addresses": {}
                     };
-                  });
-                }
-              }));
+                  } else {
+                    balances[currencyItem.symbol].balance += +value || currencyItem.value;
+                    balances[currencyItem.symbol].value += appraiser.getValue(currencyItem.value, currencyItem.symbol, currencyItem.divisible);
+                  }
+
+                  if (currencyItem.symbol == 'BTC') {
+                    balances[currencyItem.symbol].name = "Bitcoin"
+                  }
+          		    if (addr.privkey) {
+          			   hasPrivate=true;
+          		    } else {
+          			   hasPrivate=false;
+          		    }
+          		    if (addr.pubkey) {
+                   isOffline=true;
+                  } else {
+                   isOffline=false;
+                  }
+                  balances[currencyItem.symbol].addresses[addr.address] = {
+                    "address": result.data.address,
+                    "balance": +value || currencyItem.value,
+                    "value": appraiser.getValue(currencyItem.value, currencyItem.symbol, currencyItem.divisible),
+	                  "private": hasPrivate,
+	                  "offline": isOffline
+                  };
+                });
+              }
             });
             // First, the standard currencies.
-            requests.push($http.get('/v1/transaction/values.json').then(function(result) {
+            $http.get('/v1/transaction/values.json').then(function(result) {
               currencyInfo = result.data;
-            }
-            ));
-            $q.all(requests).then(function(responses) {
-              if (currencyInfo) {
-                currencyInfo.forEach(function(item) {
-                  if (balances.hasOwnProperty(item.currency))
-                    balances[item.currency].name = item.name;
-                });
+              currencyInfo.forEach(function(item) {
+                if (balances.hasOwnProperty(item.currency))
+                  balances[item.currency].name = item.name;
+              });
 
-                // Now, any applicable smart properties.
-                var spReqs = [];
+              // Now, any applicable smart properties.
+              var spReqs = [];
 
-                for (var b in balances) {
-                  var spMatch = balances[b].symbol.match(/^SP([0-9]+)$/);
-                  if (spMatch != null) {
-                    var updateFunction = function(result) {
-                      if (result.status == 200) {
-                        this.property_type = result.data[0].formatted_property_type
-                        this.name = result.data[0].propertyName + ' (' + this.symbol.match(/^SP([0-9]+)$/)[1] + ')';
-                      }
-                    };
-                    spReqs.push($http.get('/v1/property/' + spMatch[1] + '.json').then(updateFunction.bind(balances[b])));
-                  }
+              for (var b in balances) {
+                var spMatch = balances[b].symbol.match(/^SP([0-9]+)$/);
+                if (spMatch != null) {
+                  var updateFunction = function(result) {
+                    if (result.status == 200) {
+                      this.property_type = result.data[0].formatted_property_type
+                      this.name = result.data[0].propertyName + ' (' + this.symbol.match(/^SP([0-9]+)$/)[1] + ')';
+                    }
+                  };
+                  spReqs.push($http.get('/v1/property/' + spMatch[1] + '.json').then(updateFunction.bind(balances[b])));
                 }
+              }
 
-                if (spReqs.length > 0) {
-                  $q.all(spReqs).then(function() {
-                    deferred.resolve(
-                    {
-                      balances: balances,
-                      currencies: currencyInfo
-                    });
-                  });
-                } else {
+              if (spReqs.length > 0) {
+                $q.all(spReqs).then(function() {
                   deferred.resolve(
                   {
                     balances: balances,
                     currencies: currencyInfo
                   });
-                }
+                });
+              } else {
+                deferred.resolve(
+                {
+                  balances: balances,
+                  currencies: currencyInfo
+                });
               }
             });
           } else {
