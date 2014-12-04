585c5dfce0a74d95ef1a49eb8a63dbe05631cbad
Tomás Corellano <hack.rapture@gmail.com>
2014-12-04 00:48:07 -0300

    Added OrderbookFactory
    Conflicts:
    	www/index.html

---
+++ www/js/factories/OrderbookFactory.js
@@ -0,0 +1,11 @@
+angular.module("omniFactories")
+	.factory("Orderbook",[function OrderbookFactory(){
+		var Orderbook = function(transactionManager,tradingPair){
+			var self = this;
+
+			self.initialize = function(){
+				self.transactionManager=transactionManager;
+				self.tradingPair=tradingPair;
+			}
+		}
+	}]);
