2b181b36d23adbc84af068f7f6b974aeefebf589
Zifare <core_93@hotmail.com>
2015-01-17 03:09:58 -0300

    Added all the text needed for My Assets page to en-US.js

--- www/js/lang/en-US.js
+++ www/js/lang/en-US.js
@@ -3,5 +3,10 @@ angular.module("omniConfig")
 	COMMON_ADDRESS: 'Address',
    	HOMEPAGE_BALANCECHECK:'Balance Check',
 	HOMEPAGE_ENTERVALIDADDRESS:'Enter a valid Bitcoin Address, e.g. 1EXoDusjGwvnjZUyKkxZ4UHEf77z6A5S4P',
-	HOMEPAGE_BROWSERRECOMMENDATION: 'Omniwallet recommends the use of Chrome, since the wallet may not yet function perfectly on other browsers.'
+	HOMEPAGE_BROWSERRECOMMENDATION: 'Omniwallet recommends the use of Chrome, since the wallet may not yet function perfectly on other browsers.',
+	WALLET_ASSETS_ASSETS: 'Assets',
+	WALLET_ASSETS_CREATE: 'Create',
+	WALLET_ASSETS_CROWDSALE: 'Crowdsale',
+	WALLET_ASSETS_MYASSETS: 'My Assets',
+	WALLET_ASSETS_SMARTPROPERTY: 'Smart Property'
   })
