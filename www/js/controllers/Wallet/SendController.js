e476899333a111de0c69153255364fbcfa9894ab
Tomás Corellano <hack.rapture@gmail.com>
2015-01-20 17:05:28 -0300

    add SendController

---
+++ www/js/controllers/Wallet/SendController.js
@@ -0,0 +1,12 @@
+angular.module("omniControllers")
+	.controller("WalletSendController",["$scope", function WalletSendController($scope){
+		$scope.selectedAsset = $scope.wallet.getAsset(1) || $scope.wallet.getAsset(0);
+		$scope.selectedAddress = $scope.selectedAsset.tradeableAddresses[0];
+
+      	$scope.showtesteco = $scope.account.getSetting('showtesteco');
+
+      	$scope.setAsset = function(asset){
+      		$scope.selectedAsset = asset;
+      		$scope.selectedAddress = $scope.selectedAsset.tradeableAddresses[0];
+      	}
+	}])
