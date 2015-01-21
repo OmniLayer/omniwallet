2324039a88a2ca604695a438c23e34944832dbe1
Tomás Corellano <hack.rapture@gmail.com>
2015-01-21 18:38:14 -0300

    Fix typo

--- www/js/controllers/Wallet/SendController.js
+++ www/js/controllers/Wallet/SendController.js
@@ -1,7 +1,7 @@
 angular.module("omniControllers")
 	.controller("WalletSendController",["$scope", function WalletSendController($scope){
 		$scope.selectedAsset = $scope.wallet.getAsset(1) || $scope.wallet.getAsset(0);
-		$scope.selectedAddress = $scope.selectedAsset.tradeableAddresses[0];
+		$scope.selectedAddress = $scope.selectedAsset.tradableAddresses[0];
 
       	$scope.showtesteco = $scope.account.getSetting('showtesteco');
 
