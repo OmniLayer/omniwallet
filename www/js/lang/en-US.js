f72cc9d62fcc1b03940a1c1fd27368f839091bff
Zifare <core_93@hotmail.com>
2015-01-21 03:53:05 -0300

    Removed HOMEPAGE_BROWSERRECOMMENDATION from en-US.js as it shouldn't be there anymore (it was removed in the homepage refactor)

--- www/js/lang/en-US.js
+++ www/js/lang/en-US.js
@@ -3,7 +3,6 @@ angular.module("omniConfig")
 	COMMON_ADDRESS: 'Address',
    	HOMEPAGE_BALANCECHECK:'Balance Check',
 	HOMEPAGE_ENTERVALIDADDRESS:'Enter a valid Bitcoin Address, e.g. 1EXoDusjGwvnjZUyKkxZ4UHEf77z6A5S4P',
-	HOMEPAGE_BROWSERRECOMMENDATION: 'Omniwallet recommends the use of Chrome, since the wallet may not yet function perfectly on other browsers.',
 	WALLET_ASSETS_ASSETS: 'Assets',
 	WALLET_ASSETS_CREATE: 'Create',
 	WALLET_ASSETS_CROWDSALE: 'Crowdsale',
