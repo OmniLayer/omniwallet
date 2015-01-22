75056afe2a1a2e2abc89b6dbcf04109131d9f9bb
Tomás Corellano <hack.rapture@gmail.com>
2015-01-21 22:50:47 -0300

    Fix typo

--- www/js/controllers/Wallet/SendController.js
+++ www/js/controllers/Wallet/SendController.js
@@ -1,5 +1,5 @@
 angular.module("omniControllers")
-	.controller("WalletSendController",["$scope", "MIN_MINER_FEE", "OMNI_PROTOCOL_COST" function WalletSendController($scope, MIN_MINER_FEE, PROTOCOL_FEE){
+	.controller("WalletSendController",["$scope", "MIN_MINER_FEE", "OMNI_PROTOCOL_COST", function WalletSendController($scope, MIN_MINER_FEE, PROTOCOL_FEE){
 		$scope.minersFee = MIN_MINER_FEE;
 		$scope.protocolFee = PROTOCOL_FEE;
 
