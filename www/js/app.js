43c41a3f6875f0333bd37be80e5708fb94254945
Tomás Corellano <hack.rapture@gmail.com>
2015-01-29 23:56:13 -0300

    Add missing dependency

--- www/js/app.js
+++ www/js/app.js
@@ -13,7 +13,7 @@ angular.module("omniConfig")
     else
       return "https://blockchain.info/tx/";
   }]);
-  
+
 angular.module("omniFilters", ["omniConfig"]);
 angular.module("omniFactories", ["omniConfig"]);
 angular.module("omniServices", ["omniConfig", "omniFactories"]);
@@ -30,6 +30,7 @@ var app = angular.module('omniwallet', [
   'pascalprecht.translate',
   'nvd3',
   'omniConfig',
+  'omniFilters',
   'omniFactories',
   'omniServices',
   'omniControllers'
