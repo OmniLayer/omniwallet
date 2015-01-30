240cb5163670aa8eaafd34a1f4a2b0648fa3398f
Tomás Corellano <hack.rapture@gmail.com>
2015-01-30 01:36:45 -0300

    Add i18n to the full view

--- www/js/controllers/Wallet/HistoryController.js
+++ www/js/controllers/Wallet/HistoryController.js
@@ -2,14 +2,6 @@ angular.module('omniControllers')
 	.controller('WalletHistoryController', ["$scope", "$q", "$http", "hashExplorer", "$translate",
 		function WalletHistoryController($scope, $q, $http, hashExplorer, $translate) {
 		    $scope.setHashExplorer = hashExplorer.setHash.bind(hashExplorer);
-		    var alladdresses = {hash:""};
-
-		    $translate('WALLET_HISTORY_ALLADDRESSES').then(function(translation){
-		  		alladdresses={hash:translation.WALLET_HISTORY_ALLADDRESSES};
-		  		$scope.selectedAddress = alladdresses;
-	  		});
-
-
   			$scope.history = $scope.wallet.transactions();		
 
 
@@ -18,7 +10,7 @@ angular.module('omniControllers')
 			  		$scope.selectedAddress = address;
 			  		$scope.history = address.transactions;
 			  	} else {
-			  		$scope.selectedAddress = alladdresses
+			  		$scope.selectedAddress = undefined;
 			  		$scope.history = $scope.wallet.transactions();
 			  	
 			  	}
