bc973b3c193768c4b0ae39c4ebdd766dddeb47d1
Tomás Corellano <hack.rapture@gmail.com>
2015-02-01 04:59:49 -0300

    Map the list of addresses to the addresses hash to get the assets by owner

--- www/js/controllers/Wallet/AssetsController.js
+++ www/js/controllers/Wallet/AssetsController.js
@@ -1,7 +1,7 @@
 angular.module("omniControllers")
 	.controller("WalletAssetsController",["$scope", "PropertyManager", function WalletAssetsController($scope, PropertyManager){
 	    $scope.assets = [];
-	    PropertyManager.listByOwner($scope.wallet.tradableAddresses()).then(function(result){
+	    PropertyManager.listByOwner($scope.wallet.tradableAddresses().map(function(address){ return address.hash; })).then(function(result){
 	      $scope.assets=result.data.properties;
 	    });
 	  
