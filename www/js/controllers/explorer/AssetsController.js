df604e8ea7852245d035516d27e01f2be2e0606b
Tomás Corellano <hack.rapture@gmail.com>
2015-02-01 04:00:37 -0300

    Add missing dependencies to ExplorerAssetsController

--- www/js/controllers/explorer/AssetsController.js
+++ www/js/controllers/explorer/AssetsController.js
@@ -1,5 +1,5 @@
 angular.module("omniControllers")
-	.controller("ExplorerAssetsController",[function ExplorerAssetsController($scope, PropertyManager){
+	.controller("ExplorerAssetsController",["$scope", "PropertyManager", function ExplorerAssetsController($scope, PropertyManager){
 	    $scope.assets = [];
 	    PropertyManager.list().then(function(result){
 	      $scope.assets=result.data.properties;
