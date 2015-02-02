b486d2f943f85bb3adab604f64dc5dc1e7777cbc
Tomás Corellano <hack.rapture@gmail.com>
2015-02-02 03:52:15 -0300

    Add timers for remaining time

--- www/js/controllers/explorer/CrowdsaleController.js
+++ www/js/controllers/explorer/CrowdsaleController.js
@@ -1,10 +1,20 @@
 angular.module("omniControllers")
-	.controller("ExplorerCrowdsalesController",["$scope", "PropertyManager", function ExplorerCrowdsalesController($scope, PropertyManager){
-	    $scope.crowdsales = [];
-	    $scope.ecosystem = 2;
-	    PropertyManager.listCrowdsales($scope.ecosystem).then(function(result){
-	      $scope.crowdsales=result.data.properties;
-	    });
-	  
-	  
+	.controller("ExplorerCrowdsalesController",["$scope", "PropertyManager", "$timeout","$http","$templateCache","$element","$compile",
+		function ExplorerCrowdsalesController($scope, PropertyManager, $timeout, $http, $templateCache, $element, $compile){
+		    $scope.crowdsales = [];
+		    $scope.ecosystem = 2;
+		    PropertyManager.getActiveCrowdsales($scope.ecosystem).then(function(result){
+		        $scope.crowdsales=result.data.properties
+
+		        $timeout(function (){
+		        	return $http.get("/views/assets/partials/remaining_timer.html", {cache: $templateCache}).success(function(template) {
+			            for (var i = 0; i < $scope.crowdsales.length; i++) {
+		        		  
+		        			var endTime = $scope.crowdsales[i].deadline;
+		              		$element.find('#timerWrapper-'+$scope.crowdsales[i].propertyid).append(template.replace("{{endtime}}",endtime);
+		        		}
+		        		$compile($element.contents())($scope);
+				    });
+		        });;
+		    });
 	}])
