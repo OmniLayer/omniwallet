5b3147ca93be8ede18d0e9d8bc6de21ebe83bfd4
Tomás Corellano <hack.rapture@gmail.com>
2015-02-02 02:03:18 -0300

    Fix for directive timeAgo

--- www/js/filters/TimeAgo.js
+++ www/js/filters/TimeAgo.js
@@ -9,17 +9,17 @@ angular.module("omniFilters")
 		      if (off < 60)
 		        return translations.TIMEAGO_NOW;
 		      else if (off < 3600)
-		        return Math.round(off / 60) + translations.TIMEAGO_MINUTE;
+		        return Math.round(off / 60) + ' ' + translations.TIMEAGO_MINUTE;
 		      else if (off < 86400)
-		        return Math.round(off / 3600) + translations.TIMEAGO_HOUR;
+		        return Math.round(off / 3600) + ' ' + translations.TIMEAGO_HOUR;
 		      else if (off < 604800)
-		        return Math.round(off / 86400) + translations.TIMEAGO_DAY;
+		        return Math.round(off / 86400) + ' ' + translations.TIMEAGO_DAY;
 		      else if (off < 2592000)
-		        return Math.round(off / 604800) + translations.TIMEAGO_WEEK;
+		        return Math.round(off / 604800) + ' ' + translations.TIMEAGO_WEEK;
 		      else if (off < 31536000)
-		        return Math.round(off / 2592000) + translations.TIMEAGO_MONTH;
+		        return Math.round(off / 2592000) + ' ' + translations.TIMEAGO_MONTH;
 		      else
-		        return Math.round(off / 31536000) + translations.TIMEAGO_YEAR;	
+		        return Math.round(off / 31536000) + ' ' + translations.TIMEAGO_YEAR;	
 		    
 		  };
 		}])
