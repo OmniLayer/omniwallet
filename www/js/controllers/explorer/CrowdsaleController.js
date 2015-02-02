1d7b7033b5e82e094fc8af107fdf776d2f2bc033
Tomás Corellano <hack.rapture@gmail.com>
2015-02-02 03:39:45 -0300

    Add ExplorerCrowdsaleController

--- www/js/controllers/explorer/CrowdsaleController.js
+++ www/js/controllers/explorer/CrowdsaleController.js
@@ -1,5 +1,5 @@
 angular.module("omniControllers")
-	.controller("ExplorerCrowdsaleController",["$scope", "PropertyManager", function ExplorerCrowdsaleController($scope, PropertyManager){
+	.controller("ExplorerCrowdsalesController",["$scope", "PropertyManager", function ExplorerCrowdsalesController($scope, PropertyManager){
 	    $scope.crowdsales = [];
 	    $scope.ecosystem = 2;
 	    PropertyManager.listCrowdsales($scope.ecosystem).then(function(result){
