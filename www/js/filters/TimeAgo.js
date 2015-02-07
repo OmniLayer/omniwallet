1122d7673cf751ccb2efc2cfc47d601d65dacbce
Tomás Corellano <hack.rapture@gmail.com>
2015-02-07 01:31:40 -0300

    Fixes fori18n

--- www/js/filters/TimeAgo.js
+++ www/js/filters/TimeAgo.js
@@ -4,22 +4,22 @@ angular.module("omniFilters")
 		      var time = new Date(blocktime * 1000);
 		      var now = new Date();
 		      var off = (now.getTime() / 1000) - blocktime;
-		      var translations = $translate.instant(['TIMEAGO.NOW','TIMEAGO.MINUTE','TIMEAGO.HOUR','TIMEAGO.DAY','TIMEAGO.WEEK','TIMEAGO.MONTH','TIMEAGO.YEAR',])
+		      var translations = $translate.instant(['TIMEAGO.NOW','TIMEAGO.MINUTE','TIMEAGO.HOUR','TIMEAGO.DAY','TIMEAGO.WEEK','TIMEAGO.MONTH','TIMEAGO.YEAR'])
 	      
 		      if (off < 60)
-		        return translations.TIMEAGO.NOW;
+		        return translations["TIMEAGO.NOW"];
 		      else if (off < 3600)
-		        return Math.round(off / 60) + ' ' + translations.TIMEAGO.MINUTE;
+		        return Math.round(off / 60) + ' ' + translations["TIMEAGO.MINUTE"];
 		      else if (off < 86400)
-		        return Math.round(off / 3600) + ' ' + translations.TIMEAGO.HOUR;
+		        return Math.round(off / 3600) + ' ' + translations["TIMEAGO.HOUR"];
 		      else if (off < 604800)
-		        return Math.round(off / 86400) + ' ' + translations.TIMEAGO.DAY;
+		        return Math.round(off / 86400) + ' ' + translations["TIMEAGO.DAY"];
 		      else if (off < 2592000)
-		        return Math.round(off / 604800) + ' ' + translations.TIMEAGO.WEEK;
+		        return Math.round(off / 604800) + ' ' + translations["TIMEAGO.WEEK"];
 		      else if (off < 31536000)
-		        return Math.round(off / 2592000) + ' ' + translations.TIMEAGO.MONTH;
+		        return Math.round(off / 2592000) + ' ' + translations["TIMEAGO.MONTH"];
 		      else
-		        return Math.round(off / 31536000) + ' ' + translations.TIMEAGO.YEAR;	
+		        return Math.round(off / 31536000) + ' ' + translations["TIMEAGO.YEAR"];
 		    
 		  };
 		}])
