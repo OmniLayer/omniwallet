473ce02f079a009297a1c8e6638c59f9cff03b7b
Tomás Corellano <hack.rapture@gmail.com>
2015-02-09 19:25:57 -0300

    Fix typo

--- www/js/controllers/explorer/CrowdsaleController.js
+++ www/js/controllers/explorer/CrowdsaleController.js
@@ -14,7 +14,7 @@ angular.module("omniControllers")
 			        })
 			        if($scope.crowdsales.length>0)
 				        $timeout(function (){
-				        	return $http.get("/views/assets/partials/TIMER.remaining.html", {cache: $templateCache}).success(function(template) {
+				        	return $http.get("/views/assets/partials/timer_remaining.html", {cache: $templateCache}).success(function(template) {
 					            for (var i = 0; i < $scope.crowdsales.length; i++) {
 				        			var endTime = $scope.crowdsales[i].deadline;
 				              		$element.find('#timerWrapper-'+$scope.crowdsales[i].propertyid).append(template.replace("{{endtime}}",endTime));
