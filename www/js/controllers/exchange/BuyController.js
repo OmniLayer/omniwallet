65b7db871ba1cf947b4537a19a2c01a791ba627a
Tomas Corellano <tomas@mastercoin.org>
2015-03-27 19:30:00 -0300

    Fix variable in BuyController

--- www/js/controllers/exchange/BuyController.js
+++ www/js/controllers/exchange/BuyController.js
@@ -37,7 +37,7 @@ angular.module("omniControllers")
 
       $scope.confirm = function(){
         //TODO: VALIDATIONS
-        var fee = new Big($scope.minerFees);
+        var fee = new Big($scope.minersFee);
         var exchangeAccept = new Transaction(22,$scope.selectedAddress,fee,{
             amount: $scope.buyAmount,
             tx_hash: $scope.buySaleID,
@@ -51,10 +51,10 @@ angular.module("omniControllers")
             title:"EXCHANGE.BUY.MODALTITLE",
             displayedAbbreviation: $scope.displayedAbbreviation,
             buyAmount: $scope.buyAmount,
-            minerFees: $scope.minerFees,
+            minerFees: $scope.minersFee,
             selectedCoin: $scope.selectedCoin,
             insufficientBitcoin : $scope.insufficientBitcoin,
-            fees : $scope.minerFees,
+            fees : $scope.minersFee,
             totalCost : exchangeAccept.totalCost,
             confirmText:"EXCHANGE.BUY.CONFIRM",
             explorerUrl:ADDRESS_EXPLORER_URL,
