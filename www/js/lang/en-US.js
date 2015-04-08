b023c1836f530b69d39352320abecff0efdb689e
Tomas Corellano <tomas@mastercoin.org>
2015-04-08 04:49:10 -0300

    i18n on mastercoin about page

--- www/js/lang/en-US.js
+++ www/js/lang/en-US.js
@@ -52,7 +52,27 @@ angular.module("omniConfig")
             "CHATDESC":"Chat with the developers, share your ideas or ask questions. Availability varies, but we should generally be around Mon-Fri 9-5 CDT",
             "TITLE":"Contact Us"
 		},
-		"MASTERCOIN":{},
+		"MASTERCOIN":{
+			"TITLE":"About Mastercoin",
+			"FOUNDATION":{
+				"TITLE":"Mastercoin Foundation",
+				"FIRST":"In September 2013, the Mastercoin Foundation was formed to temporarily manage the funds in the Exodus Address and the distribution of the development MSC. The board of the Mastercoin Foundation has declared its intention to minimize its temporary central role by transitioning the decision making to Mastercoin owners through proof of stake voting.",
+				"SECOND": "The Mastercoin Foundation advocates for the use of the Master Protocol and tries to build a community of people who develop it. All budget items, board minutes, Dev MSC vesting schedule, and bounties are public record and available to anyone on the "
+				"LINK": "Foundation's Website"
+			},
+			"PROTOCOL":{
+				"TITLE":"Master Protocol",
+				"PARAGRAPHS":"The Master Protocol is a communications protocol that uses the Bitcoin block chain to enable features such as smart contracts, user currencies and decentralized peer-to-peer exchanges. A common analogy that is used to describe the relation of the Master Protocol to Bitcoin is that of HTTP to TCP/IP: HTTP, like the Master Protocol, is the application layer to the more fundamental transport and internet layer of TCP/IP, like Bitcoin. For more in-depth information and details see the "
+				"LINK":"<a href=\"https://github.com/mastercoin-MSC/spec#the-master-protocol--mastercoin-complete-specification\" target=\"_new\"><b>Spec</b></a> on github."
+
+			},
+			"TOKEN":{
+				"TITLE":"Mastercoins",
+				"FIRST" : "Mastercoins (symbol MSC) are digital tokens that are necessary for the use of some features of the Master Protocol. The total number of Mastercoins in existence is 619,478.6 and no more MSC will ever be created. Additionally, Mastercoins can not be mined into existence. The 619,478.6 MSC were generated as a result of a public fundraiser in the style of Kickstarter.com. For more details about the algorithm by which Mastercoins were generated, please read the ",
+				"SECOND" : "were generated, please read the <a href=\"http://masterprotocoleducation.org/history-of-the-master-protocol/\" target=\"_new\"><b>History of Mastercoin</b></a>article from Master Protocol Education site. ",
+				"LINK":"Information from the <a href=\"http://masterprotocoleducation.org/\" target=\"_new\"><b>Master Protocol Education</b></a>"
+			}
+		},
 		"OMNIWALLET":{},
 		"FAQ":{}
 	},
