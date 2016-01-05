d6675b9e79ee71043c83e78e09907c5e84c1247e
Tomas Corellano <corellanot@gmail.com>
2016-01-05 00:25:11 -0300

    Show prices in balance check

--- www/js/controllers/landing/HomeController.js
+++ www/js/controllers/landing/HomeController.js
@@ -1,6 +1,6 @@
 angular.module("omniControllers")
-  .controller("HomeController", ["$scope","$templateCache", "$injector", "$location","$http", "$q", "Account", "Address", "appraiser",
-    function HomeController($scope, $templateCache, $injector, $location, $http, $q, Account, Address, appraiser) {
+  .controller("HomeController", ["$scope","$templateCache", "$injector", "$location","$http", "$q", "Account", "Address", 
+    function HomeController($scope, $templateCache, $injector, $location, $http, $q, Account, Address) {
     if (Account.uuid) {
       $location.url('/wallet/overview');
     } else {
@@ -8,6 +8,7 @@ angular.module("omniControllers")
       console.log('cleared cache');
       $templateCache.removeAll();
 
+      $scope.conversions = {};
       $scope.balanceAddress = ""; 
       $scope.checkAddress = null;
       $scope.total = 0;
@@ -22,6 +23,53 @@ angular.module("omniControllers")
         //call modal manager service to display the balance check modal
       }
 
-      $scope.getValue = appraiser.getValue
+      $scope.getValue = function(amount, symbol, divisible) {
+        if (symbol == 'BTC') {
+          if ($scope.conversions.BTC)
+            return $scope.conversions.BTC * amount;
+          else
+            return 0;
+        } else {        
+          if ($scope.conversions.hasOwnProperty(symbol)) {
+            if (divisible)
+              return $scope.getValue($scope.conversions[symbol] * amount, 'BTC', true);
+            else
+              return $scope.getValue($scope.conversions[symbol] * amount * 100000000, 'BTC', true);
+          } else
+            return 0;
+        }
+      };
+
+      $scope.$on("address:loaded",function(evt,args){
+        var requests = [];
+        var coins = $scope.checkAddress.balance;
+        cursym = "USD";
+        coins.forEach(function(coin) {
+          var symbol = "";
+          if (coin.symbol === 'BTC') {
+            symbol="BTC"+cursym;
+          } else {
+            symbol=coin.symbol;
+          }
+          requests.push(
+          $http.get('/v1/values/' + symbol + '.json').then(function(response) {
+            var currency = response.data[0];
+            if (currency.symbol == 'BTC') {
+              // Store these things internally as the value of a satoshi.
+              $scope.conversions.BTC = currency.price / 100000000;
+            } else {
+              $scope.conversions[currency.symbol] = currency.price;
+            }
+            coin.balance = $scope.getValue(coin.balance,coin.symbol,coin.divisible)
+            coin.price = $scope.getValue(coin.divisible ? 100000000 : 1,coin.symbol,coin.divisible)
+          }, function(error) {
+            console.log(error);
+          })
+          );
+        });
+        $q.all(requests).then(function(responses) {
+          $scope.pricesLoaded = true;
+        });
+      })
      };
     }])
