9a0a1f413df34fa55a86160690c811f847521df5
Tomas Corellano <corellanot@gmail.com>
2016-01-28 19:53:09 -0300

    Fix for toWhole filter

--- www/js/filters/ToWhole.js
+++ www/js/filters/ToWhole.js
@@ -1,6 +1,7 @@
 angular.module("omniFilters")
 	.filter("toWhole",["WHOLE_UNIT", function(WHOLE_UNIT) {
 		  return function(input, isDivisible) {
+		  	if(input == 'None') input = 0;
 		    if(isDivisible){
     		    input = input || 0;
     		    var out = new Big(input);
