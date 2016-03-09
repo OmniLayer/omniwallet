d1a24797eaa3bfab36244bc1b0826e7a0c3faf25
Tomas Corellano <corellanot@gmail.com>
2016-03-09 18:44:36 -0300

    Use omnichest for address lookup

--- www/js/app.js
+++ www/js/app.js
@@ -17,7 +17,7 @@ angular.module("omniConfig")
     if(TESTNET)
       return "http://tbtc.blockr.io/address/info/";
     else
-      return "https://blockchain.info/address/";
+      return "http://omnichest.info/lookupadd.aspx?address=";
   }])
   .factory("EXODUS_ADDRESS",["TESTNET", function AddressExplorerUrlFactory(TESTNET){
     if(TESTNET)
