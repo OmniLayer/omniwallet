17375542b66a49cd5e2b81f7a977572b14d8af8e
Tomas Corellano <corellanot@gmail.com>
2016-06-10 02:26:19 -0300

    Initialize overview controller

--- www/js/controllers/DEx/OverviewController.js
+++ www/js/controllers/DEx/OverviewController.js
@@ -3,20 +3,9 @@ angular.module("omniControllers")
 		function DExOverviewController($scope,Account,Orderbook,Wallet,PropertyManager){
 			$scope.isLoggedIn = Account.isLoggedIn;
 			$scope.markets = [];
-
+			$scope.noMarkets = true;
 			$scope.setEcosystem = function(ecosystem){
 				$scope.ecosystem = ecosystem;
-			    PropertyManager.listByEcosystem($scope.ecosystem).then(function(result){
-			      var availableTokens = result.data.properties.sort(function(a, b) {
-			          var currencyA = a.name.toUpperCase();
-			          var currencyB = b.name.toUpperCase();
-			          return (currencyA < currencyB) ? -1 : (currencyA > currencyB) ? 1 : (a.propertyid < b.propertyid) ? -1 : (a.propertyid > b.propertyid) ? 1 : 0 ;
-			      });
-			      $scope.availableTokens = availableTokens.filter(function(currency){
-			        return $scope.tradingTokens.indexOf(currency.propertyid) == -1;
-			      });
-			      $scope.property=$scope.availableTokens[0];
-			    });
 			};
 
 	}]);
