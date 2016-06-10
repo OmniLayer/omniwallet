bf0f8843a7334de3bdbbfb9d78a351644d229243
Tomas Corellano <corellanot@gmail.com>
2016-06-10 02:48:45 -0300

    Add initial markets API

---
+++ api/markets.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify, abort, json, make_response
+
+app = Flask(__name__)
+app.debug = True
+
+
+
+@app.route('/designatingcurrencies', methods=['POST'])
+def getDesignatingCurrencies(ecosystem):
+	ecosystem = re.sub(r'\W+', '', ecosystem) #check alphanumeric
+	designating_currencies = dbSelect("select distinct ao.propertyiddesired, sp.propertyname from activeoffers ao inner join SmartProperties sp on ao.propertyiddesired = sp.propertyid and sp.ecosystem = %s where ao.propertyidselling not in (1, 2, 31)  order by ao.propertyiddesired ",[ecosystem])
+
+	return jsonify({"status" : 200, "currencies": designating_currencies})
