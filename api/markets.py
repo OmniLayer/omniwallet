8d7097bcd35a57118332f79c4cda342ab00578b3
Tomas Corellano <corellanot@gmail.com>
2016-06-15 23:52:19 -0300

    Fix markets API call

--- api/markets.py
+++ api/markets.py
@@ -6,8 +6,8 @@ app.debug = True
 
 
 @app.route('/designatingcurrencies', methods=['POST'])
-def getDesignatingCurrencies(ecosystem):
-	ecosystem = re.sub(r'\W+', '', ecosystem) #check alphanumeric
+def getDesignatingCurrencies():
+	ecosystem = re.sub(r'\W+', '', request.form["ecosystem"]) #check alphanumeric
 	designating_currencies = dbSelect("select distinct ao.propertyiddesired, sp.propertyname from activeoffers ao inner join SmartProperties sp on ao.propertyiddesired = sp.propertyid and sp.ecosystem = %s where ao.propertyidselling not in (1, 2, 31)  order by ao.propertyiddesired ",[ecosystem])
 
 	return jsonify({"status" : 200, "currencies": designating_currencies})
