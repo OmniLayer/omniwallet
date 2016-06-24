d3d48bccee478b62c4213baad4335126614699e9
Tomas Corellano <corellanot@gmail.com>
2016-06-24 04:52:11 -0300

    Fix for route parameters

--- www/js/controllers/DEx/OrderbookController.js
+++ www/js/controllers/DEx/OrderbookController.js
@@ -1,13 +1,12 @@
 angular.module("omniControllers")
-	.controller("DExOrderbookController", ["$scope","Account","Orderbook","Wallet","PropertyManager","$http","propertyiddesired","propertyidselling","ecosystem",
-		function DExOrderbookController($scope,Account,Orderbook,Wallet,PropertyManager,$http,propertyiddesired,propertyidselling,ecosystem){
+	.controller("DExOrderbookController", ["$scope","Account","Orderbook","Wallet","PropertyManager","$http","$route",
+		function DExOrderbookController($scope,Account,Orderbook,Wallet,PropertyManager,$http,$route){
 			$scope.isLoggedIn = Account.isLoggedIn;
 			$scope.orderbook = [];
 			$scope.noOrders = true;
-			$scope.ecosystem = ecosystem;
-			PropertyManager.getProperty(propertyiddesired).then(function(result){
+			PropertyManager.getProperty($route.current.params.propertyiddesired).then(function(result){
 				$scope.propertyDesired = result.data;
-				PropertyManager.getProperty(propertyidselling).then(function(result){
+				PropertyManager.getProperty($route.current.params.propertyidselling).then(function(result){
 					$scope.propertySelling = result.data;
 					$scope.orderbook = new Orderbook({desired:$scope.propertyDesired,selling:$propertySelling});
 				});
