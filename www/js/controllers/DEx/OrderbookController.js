d80d221174d3d56a2ec62e5e9ae28e7ab9191029
Tomas Corellano <corellanot@gmail.com>
2016-06-24 05:53:57 -0300

    Fix Orderbookcontroller

--- www/js/controllers/DEx/OrderbookController.js
+++ www/js/controllers/DEx/OrderbookController.js
@@ -6,7 +6,7 @@ angular.module("omniControllers")
 			$scope.noOrders = true;
 			PropertyManager.getProperty($route.current.params.propertyIdDesired).then(function(result){
 				$scope.propertyDesired = result.data;
-				PropertyManager.getProperty($route.current.params.propertyidselling).then(function(result){
+				PropertyManager.getProperty($route.current.params.propertyIdSelling).then(function(result){
 					$scope.propertySelling = result.data;
 					$scope.orderbook = new Orderbook({desired:$scope.propertyDesired,selling:$propertySelling});
 				});
