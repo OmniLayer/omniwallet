bf1be7d2345934b4418aff843836c49eff7853e5
Tomas Corellano <corellanot@gmail.com>
2016-06-24 09:09:44 -0300

    fix for SaleController

--- www/js/controllers/DEx/SaleController.js
+++ www/js/controllers/DEx/SaleController.js
@@ -1,7 +1,7 @@
 angular.module("omniControllers")
-	.controller("DExSaleController", ["$scope","TransactionManager","WalletAssets","ModalManager", 
-		function DExSaleController($scope, TransactionManager, WalletAssets, ModalManager ){
-			$scope.walletAssets = WalletAssets;
+	.controller("DExSaleController", ["$scope","TransactionManager","Wallet","ModalManager", 
+		function DExSaleController($scope, TransactionManager, Wallet, ModalManager ){
+			$scope.walletAssets = Wallet;
 
 			$scope.DExSaleTransaction = new TransactionManager(25,{
 						transaction_version:0,
@@ -11,15 +11,16 @@ angular.module("omniControllers")
 						desired_amount:$scope.desiredAmount
 					});
 
-			$scope.setSelectedCoin = function(currencyId){
-				WalletAssets.currencyList.forEach(function(currency){
-					if(currency.id == currencyId.toString())
-						WalletAssets.selectedCoin = currency;
-				});
-			}
-
+			
 			$scope.validateDexSaleForm = function(){
 				// TODO: Validations
+				$scope.DExSaleTransaction = new TransactionManager(25,{
+						transaction_version:0,
+						sale_currency_id:$scope.saleCurrency,
+						sale_amount:$scope.saleAmount,
+						desired_currency_id:$scope.desiredCurrency,
+						desired_amount:$scope.desiredAmount
+					});
 				ModalManager.openConfirmationModal({
 					dataTemplate: '/views/modals/partials/dex_sale.html',
 					scope: {
