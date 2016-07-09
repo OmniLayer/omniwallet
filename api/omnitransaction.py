fdc49ad15c812d336ef05994274186912fdf141b
Tomas Corellano <corellanot@gmail.com>
2016-07-09 18:53:41 -0300

    Fix not enough funds error

--- api/omnitransaction.py
+++ api/omnitransaction.py
@@ -70,7 +70,7 @@ class OmniTransaction:
             return { "status": "NOT OK", "error": "Couldn't get list of unspent tx's. Response Code: " + str(dirty_txes['code'])  }
 
         if (dirty_txes['error'][:3]=='Low'):
-            return { "status": "NOT OK", "error": "Not enough funds, try again. Needed: " + str(fee_total) + " but Have: " + str(dirty_txes['avail'])  }
+            return { "status": "NOT OK", "error": "Not enough funds, try again. Needed: " + str(fee_total) + " but Have: " + str(dirty_txes['avail'] / Decimal(1e8))  }
 
         total_amount = dirty_txes['avail']
         unspent_tx = dirty_txes['utxos']
