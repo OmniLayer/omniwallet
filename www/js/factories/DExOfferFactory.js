2babae74a5b81ef212a5db1b0e98a66376d0cab0
Adam <achamely@users.noreply.github.com>
2016-09-26 20:12:27 -0400

    update/fix cancel for asks

--- www/js/factories/DExOfferFactory.js
+++ www/js/factories/DExOfferFactory.js
@@ -13,10 +13,11 @@ angular.module("omniFactories")
 				self.selling_amount = propertyselling.divisible ? new Big(data.total_amount).times(WHOLE_UNIT) : new Big(data.total_amount);
 				self.total_desired_amount = propertydesired.divisible ? new Big(data.desired_amount).times(WHOLE_UNIT) : new Big(data.desired_amount);
 				self.desired_amount = self.total_desired_amount;
+				local_price = new Big(data.unit_price);
 				if(side == "ask"){
+					self.total_desired_amount = new Big(self.selling_amount).times(local_price);
 					self.price = self.desired_amount.div(self.available_amount);
 				} else if (side == "bid"){
-					local_price = new Big(data.unit_price);
 					self.desired_amount = new Big(self.available_amount).times(local_price);
 					self.price = self.available_amount.div(self.desired_amount);
 				} else if(side = "market"){
