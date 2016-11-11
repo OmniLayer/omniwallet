03abd0df032675cd3c7ec07ae0856b4b6c98bc5d
Adam <adam@mastercoin.org>
2016-11-11 14:53:48 -0600

    Fix order rounding, add order details

--- www/js/factories/DExOfferFactory.js
+++ www/js/factories/DExOfferFactory.js
@@ -9,6 +9,7 @@ angular.module("omniFactories")
 				self.rawdata = data;
 				self.propertyselling = propertyselling;
 				self.propertydesired = propertydesired;
+				self.txhash = data.txhash;
 				local_price = new Big(data.unit_price);
 				//original amount selling
 				self.selling_amount = propertyselling.divisible ? new Big(data.total_amount).times(WHOLE_UNIT) : new Big(data.total_amount);
@@ -16,16 +17,17 @@ angular.module("omniFactories")
 				self.available_amount = propertyselling.divisible ? new Big(data.available_amount).times(WHOLE_UNIT) : new Big(data.available_amount);
 				//original amount desired
 				self.total_desired_amount = propertydesired.divisible ? new Big(data.desired_amount).times(WHOLE_UNIT) : new Big(data.desired_amount);
-				self.desired_amount = self.total_desired_amount;
+				//amount desired to fill
+				self.desired_amount = new Big(Math.ceil(self.available_amount.times(local_price).div(WHOLE_UNIT).toString())).times(WHOLE_UNIT);
+				//protect against price drift when no matches have been made yet
+				if (self.desired_amount > self.total_desired_amount) {
+					self.desired_amount = self.total_desired_amount;
+				}
 
 				if(side == "ask"){
-					self.total_desired_amount = new Big(self.available_amount.times(local_price).toFixed(8));
 					self.price = new Big(Math.ceil(self.desired_amount.div(self.available_amount).div(WHOLE_UNIT).toString())).times(WHOLE_UNIT);
-					//self.price = self.local_price;
 				} else if (side == "bid"){
-					self.desired_amount = new Big(self.available_amount.times(local_price).toFixed(8));
-					self.price = new Big(Math.floor(self.available_amount.div(self.total_desired_amount).div(WHOLE_UNIT).toString())).times(WHOLE_UNIT);
-					//self.price = self.available_amount.div(self.desired_amount);
+					self.price = new Big(Math.floor(self.available_amount.div(self.desired_amount).div(WHOLE_UNIT).toString())).times(WHOLE_UNIT);
 				} else if(side = "market"){
 					self.price = new Big(data.unit_price);
 				}
