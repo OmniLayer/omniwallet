7b7aa4f59bbe4d7aa32b02e21673a4b8e20e6729
Adam <adam@mastercoin.org>
2017-07-12 09:14:23 -0500

    configurable expire time

--- api/cacher.py
+++ api/cacher.py
@@ -4,6 +4,11 @@ import json
 
 r = redis.StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB)
 
+try:
+  expTime=config.BTCBAL_CACHE
+except:
+  expTime=600
+
 def rGet(key):
   return r.get(key)
 
@@ -24,7 +29,7 @@ def rSetNotUpdateBTC(baldata):
   if fresh!=None and len(fresh)>0:
     for addr in fresh:
       rSet("omniwallet:balances:address:"+str(addr),json.dumps( {"bal":baldata['bal'][addr],"error":None}))
-      rExpire("omniwallet:balances:address:"+str(addr),150)
+      rExpire("omniwallet:balances:address:"+str(addr),expTime)
 
 def rExpireAllBalBTC():
   for addr in rKeys("omniwallet:balances:address:*"):
