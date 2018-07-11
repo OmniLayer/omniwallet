a1323d319975588dd801f3f14933b9a1b26caace
Adam <adam@mastercoin.org>
2018-07-11 18:11:24 -0500

    cleanup send and remove / consolidate deprecated dependancies/calls

--- api/send.py
+++ api/send.py
@@ -1,14 +1,21 @@
 import urlparse
 import os, sys
-tools_dir = os.environ.get('TOOLSDIR')
-lib_path = os.path.abspath(tools_dir)
-sys.path.append(lib_path)
-from msc_utils_parsing import *
+#tools_dir = os.environ.get('TOOLSDIR')
+#lib_path = os.path.abspath(tools_dir)
+#sys.path.append(lib_path)
+#from msc_utils_parsing import *
+from common_utils import *
 from blockchain_utils import *
 from msc_apps import *
 import random
 
+max_currency_value=21000000
+dust_limit=5757
+
 def send_form_response(response_dict):
+    print "send form response dict"
+    print response_dict
+
     expected_fields=['from_address', 'to_address', 'amount', 'currency', 'fee']
     # if marker is True, send dust to marker (for payments of sells)
     for field in expected_fields:
@@ -26,21 +33,20 @@ def send_form_response(response_dict):
         testnet = False
         magicbyte = 0
 
-    if response_dict.has_key( 'pubKey' ) and is_pubkey_valid( response_dict['pubKey'][0]):
+    if response_dict.has_key( 'pubKey' ): #and is_pubkey_valid( response_dict['pubKey'][0]):
         pubkey = response_dict['pubKey'][0]
         response_status='OK'
     else:
         response_status='invalid pubkey'
         pubkey=None
-    
-    print response_dict
+    #max btc amount
   
     from_addr=response_dict['from_address'][0]
-    if not is_valid_bitcoin_address_or_pubkey(from_addr):
-        return (None, 'From address is neither bitcoin address nor pubkey')
+    #if not is_valid_bitcoin_address_or_pubkey(from_addr):
+    #    return (None, 'From address is neither bitcoin address nor pubkey')
     to_addr=response_dict['to_address'][0]
-    if not is_valid_bitcoin_address(to_addr):
-        return (None, 'To address is not a bitcoin address')
+    #if not is_valid_bitcoin_address(to_addr):
+    #    return (None, 'To address is not a bitcoin address')
     amount=response_dict['amount'][0]
     if float(amount)<0 or float( from_satoshi(amount))>max_currency_value:
         return (None, 'Invalid amount: ' + str( from_satoshi( amount )) + ', max: ' + str( max_currency_value ))
@@ -75,21 +81,21 @@ def send_form_response(response_dict):
         tx_to_sign_dict={'transaction':'','sourceScript':''}
         l=len(from_addr)
         if l == 66 or l == 130: # probably pubkey
-            if is_pubkey_valid(from_addr):
+            #if is_pubkey_valid(from_addr):
                 pubkey=from_addr
                 response_status='OK'
-            else:
-                response_status='invalid pubkey'
+            #else:
+            #    response_status='invalid pubkey'
         else:   
-            if not is_valid_bitcoin_address(from_addr):
-                response_status='invalid address'
-            else:
+            #if not is_valid_bitcoin_address(from_addr):
+            #    response_status='invalid address'
+            #else:
                 from_pubkey=bc_getpubkey(from_addr)
-                if not is_pubkey_valid(from_pubkey):
-                    response_status='missing pubkey'
-                else:
-                    pubkey=from_pubkey
-                    response_status='OK'
+                #if not is_pubkey_valid(from_pubkey):
+                #    response_status='missing pubkey'
+                #else:
+                pubkey=from_pubkey
+                response_status='OK'
 
     try:
       if pubkey != None:
@@ -136,8 +142,9 @@ def prepare_send_tx_for_signing(from_address, to_address, marker_address, curren
         if marker_address != None:
             required_value+=1*dust_limit
     else:
-        tx_type=0 # only simple send is supported
-        required_value=4*dust_limit
+        #tx_type=0 # only simple send is supported
+        #required_value=4*dust_limit
+        raise Exception({ "status": "NOT OK", "error": "Deprecated. Please use /v1/transaction/getunsigned" })
  
     #------------------------------------------- New utxo calls
     fee_total_satoshi=required_value+fee
@@ -147,7 +154,7 @@ def prepare_send_tx_for_signing(from_address, to_address, marker_address, curren
         raise Exception({ "status": "NOT OK", "error": "Could not get list of unspent txs. Response Code: " + str(dirty_txes['code']) })
 
     if (dirty_txes['error'][:3]=='Low'):
-        raise Exception({ "status": "NOT OK", "error": "Not enough funds, try again. Needed: " + str(fee_total_satoshi) + " but Have: " + str(dirty_txes['avail'])  })
+        raise Exception({ "status": "NOT OK", "error": "Not enough funds, for " + str(from_address) + ". Needed: " + str(fee_total_satoshi) + " but Have: " + str(dirty_txes['avail'])  })
 
     inputs_total_value = dirty_txes['avail']
     inputs = dirty_txes['utxos']
@@ -206,43 +213,7 @@ def prepare_send_tx_for_signing(from_address, to_address, marker_address, curren
         
     else:
         #DEPRECATED - ALL OMNI TX Created by tx_generate_service - 
-        # create multisig tx
-        # simple send - multisig
-        # dust to exodus
-        # dust to to_address
-        # double dust to rawscript "1 [ change_address_pub ] [ dataHex_obfuscated ] 2 checkmultisig"
-        # change to change
-
-        dataSequenceNum=1
-        dataHex = '{:02x}'.format(0) + '{:02x}'.format(dataSequenceNum) + \
-                '{:08x}'.format(tx_type) + '{:08x}'.format(currency_id) + \
-                '{:016x}'.format(satoshi_amount) + '{:06x}'.format(0)
-        dataBytes = dataHex.decode('hex_codec')
-        dataAddress = hash_160_to_bc_address(dataBytes[1:21])
-
-        # create the BIP11 magic 
-        change_address_compressed_pub=get_compressed_pubkey_format( change_address_pub )
-        obfus_str=get_sha256(from_address)[:62]
-        padded_dataHex=dataHex[2:]+''.zfill(len(change_address_compressed_pub)-len(dataHex))[2:]
-        dataHex_obfuscated=get_string_xor(padded_dataHex,obfus_str).zfill(62)
-        random_byte=hex(random.randrange(0,255)).strip('0x').zfill(2)
-        hacked_dataHex_obfuscated='02'+dataHex_obfuscated+random_byte
-        info('plain dataHex: --'+padded_dataHex+'--')
-        info('obfus dataHex: '+hacked_dataHex_obfuscated)
-        valid_dataHex_obfuscated=get_nearby_valid_pubkey(hacked_dataHex_obfuscated)
-        info('valid dataHex: '+valid_dataHex_obfuscated)
-        script_str='1 [ '+change_address_pub+' ] [ '+valid_dataHex_obfuscated+' ] 2 checkmultisig'
-        info('change address is '+changeAddress)
-        info('too_address is '+to_address)
-        info('total inputs value is '+str(inputs_total_value))
-        info('fee is '+str(fee))
-        info('dust limit is '+str(dust_limit))
-        info('BIP11 script is '+script_str)
-        dataScript=rawscript(script_str)
-
-        inputs_outputs+=' -o '+exodus_address+':'+str(dust_limit) + \
-                        ' -o '+to_address+':'+str(dust_limit) + \
-                        ' -o '+dataScript+':'+str(2*dust_limit)
+        raise Exception({ "status": "NOT OK", "error": "Deprecated. Please use /v1/transaction/getunsigned" })
 
 
     if change_value >= dust_limit:
@@ -253,8 +224,12 @@ def prepare_send_tx_for_signing(from_address, to_address, marker_address, curren
         pass
 
     #tx=mktx(inputs_outputs)
-    tx=pybitcointools.mktx(ins,outs)
     info('inputs_outputs are '+str(ins)+' '+str(outs))
+    try:
+      tx=pybitcointools.mktx(ins,outs)
+    except:
+      raise Exception({ "status": "NOT OK", "error": "Invalid Bitcoin address, please check and try again" })
+
     #info('inputs_outputs are '+inputs_outputs)
     info('tx is '+str(tx))
 
